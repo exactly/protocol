4d4739893e21cce0db1e91753622e965af64aab5
Lucas <lucas.lain@gmail.com>
2021-05-18 11:04:28 +0200

    Removed comments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,6 @@ import "@openzeppelin/hardhat-upgrades";
 import "solidity-coverage";
 import "hardhat-abi-exporter";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
