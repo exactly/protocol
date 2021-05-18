5ec55865ef1c37dcda5e68f35f24fce9ad57321c
Lucas <lucas.lain@gmail.com>
2021-05-18 11:00:03 +0200

    HardhatUserConfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import { task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-solhint";
 import "@openzeppelin/hardhat-upgrades";
@@ -15,10 +16,7 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-export default {
-  solidity: "0.8.0",
+const config: HardhatUserConfig = {
+    solidity: "0.8.0",
 };
-
+export default config;
