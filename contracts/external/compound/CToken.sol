e248a040e2e8366b65d74b8e4e6c1b0af3f6485d
Lucas <lucas.lain@gmail.com>
2021-08-05 13:16:55 +0200

    Adjusting mock contract

--- contracts/external/compound/CToken.sol
+++ contracts/external/compound/CToken.sol
@@ -4,26 +4,54 @@ pragma solidity ^0.8.0;
 import "../../interfaces/ICToken.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ @dev this class is only for mocking Compound's cToken contract
+ */
 contract CToken is ICToken, ERC20 {
-    constructor (string memory name_, string memory symbol_) ERC20 (name_, symbol_) {}
+
+    constructor (string memory name_, string memory symbol_, address daiAddress) ERC20 (name_, symbol_) {
+        dai = IERC20(daiAddress);
+    }
+
+    mapping(address => uint256) public balance;
+
+    uint256 constant public UNIT = 1e18;
+
+    IERC20 dai;
 
     function exchangeRateCurrent() external override returns (uint256) {
-        return 1.2 * 10e18;
-    }
-
-    function supplyRatePerBlock() external override returns (uint256) {
-        return 100;
+        // silence warning "convert to pure" and simulate cToken's gas consumption
+        balance[msg.sender] = balance[msg.sender];
+        return 12 * UNIT / 10;
     }
     
+    /**
+     @dev Redeem the amount of cTokens to the underlaying ERC20
+     */
     function redeem(uint amount) external override returns (uint) {
-        return 100;
+        uint balanceAmount = balance[msg.sender];
+        require(balanceAmount - amount > 0, "Not enough balance");
+        balance[msg.sender] = balanceAmount - amount;
+        dai.transfer(msg.sender, balanceAmount * this.exchangeRateCurrent());
+        return 0;
     }
 
+    /**
+     @dev Redeem the amount of cTokens(in DAI) to the underlaying ERC20
+     */
     function redeemUnderlying(uint amount) external override returns (uint) {
-        return 100;
+        uint balanceAmount = balance[msg.sender];
+        require(balanceAmount * this.exchangeRateCurrent() - amount > 0, "Not enough balance");
+        uint amountDai = amount / this.exchangeRateCurrent();
+        balance[msg.sender] = balanceAmount - amount;
+        dai.transfer(msg.sender, amountDai);
+        return 0;
     }
 
     function mint(uint256 amount) external override returns (uint256) {
-        super.mint(amount);
+        dai.transferFrom(msg.sender, address(this), amount);
+        uint oldBalance = balance[msg.sender];
+        balance[msg.sender] = oldBalance + amount;
+        return amount;
     }
 }
--- contracts/interfaces/ICToken.sol
+++ contracts/interfaces/ICToken.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface ICToken is IERC20 {
     function mint(uint256) external returns (uint256);
     function exchangeRateCurrent() external returns (uint256);
-    function supplyRatePerBlock() external returns (uint256);
     function redeem(uint) external returns (uint);
     function redeemUnderlying(uint) external returns (uint);
 }
