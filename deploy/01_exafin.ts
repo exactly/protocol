31a1691f238a8f2e2ec4445874691687ccc154e4
Lucas <lucas.lain@gmail.com>
2021-08-21 10:49:59 +0200

    Switch to unnamed

--- deploy/01_exafin.ts
+++ deploy/01_exafin.ts
@@ -2,17 +2,12 @@ import { utils } from 'ethers'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
-const { parseEther } = utils
-
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments
-  const { deployer } = await hre.getNamedAccounts()
-
+  const [deployer, tokenOwner] = await hre.getUnnamedAccounts()
   await deploy('Exafin', {
     from: deployer,
-    args: [
-      "0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658"
-    ],
+    args: ["0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658"],
     log: true,
   })
 }
