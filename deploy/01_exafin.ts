797b197b2b29cdf81823bb9e7d3e27d65e73f0a4
Lucas <lucas.lain@gmail.com>
2021-08-22 08:19:20 +0200

    New version of Hardhat-Deploy
    Deployed to rinkeby

--- deploy/01_exafin.ts
+++ deploy/01_exafin.ts
@@ -4,16 +4,26 @@ import { DeployFunction } from 'hardhat-deploy/types'
 import fs from 'fs'
 import YAML from 'yaml'
 
-const file = fs.readFileSync('./deploy/config.yml', 'utf8')
-const config = YAML.parse(file)
-
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const file = fs.readFileSync('./config.yml', 'utf8')
+  const config = YAML.parse(file)
+
   const [deployer] = await hre.getUnnamedAccounts()
-  const exafin = await hre.deployments.deploy('Exafin', {
-    from: deployer,
-    args: ["0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658"], // TODO: replace this for the value in config
-    log: true
-  })
+
+  console.log(hre.network.name)
+
+  let tokensForNetwork = config.token_addresses[hre.network.name]
+  for (const [tokenName, tokenAddress] of Object.entries(tokensForNetwork)) { 
+
+    const exafin = await hre.deployments.deploy('Exafin', {
+      from: deployer,
+      args: [tokenAddress],
+      log: true
+    })  
+    console.log("Exafin %s deployed to: %s", tokenName, exafin.address)
+  }
+
+
 }
 
 func.skip = (hre: HardhatRuntimeEnvironment) => Promise.resolve(hre.network.name === 'mainnet')
