6653011b2a0d50419f6ecc033709ef7f971e35c8
Juan Gallo <ji.gallo93@gmail.com>
2021-09-15 15:23:09 -0300

    Adds impersonate for multiple tokens

--- deploy/01_exafin.ts
+++ deploy/01_exafin.ts
@@ -5,21 +5,6 @@ import fs from "fs";
 import YAML from "yaml";
 import { ethers } from "hardhat";
 
-let tokensCollateralRate = new Map([
-  ["DAI", parseUnits("0.8", 18)],
-  ["WETH", parseUnits("0.7", 18)],
-  ["USDT", parseUnits("0.4", 18)],
-]);
-
-// We're doing a little trick here. Compound's oracle has ETH (not wrapped ETH).
-// but we're going to use all ERC20's in our EXAFIN's contracts, so we're passing
-// the name for the oracle as an argument to the contracts
-let nameForOracle = new Map([
-  ["DAI", "DAI"],
-  ["WETH", "ETH"],
-  ["USDT", "USDT"],
-]);
-
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const file = fs.readFileSync("./config.yml", "utf8");
   const config = YAML.parse(file);
@@ -33,15 +18,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const exaFront = await hre.deployments.deploy("ExaFront", {
     from: deployer,
     args: [priceOracleAddress],
-    log: true,
+    log: true
   });
 
-  async function impersonate(
-    address = "0x2c8fbb630289363ac80705a1a61273f76fd5a161"
-  ) {
+  async function impersonate(address: string) {
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: [address],
+      params: [address]
     });
 
     const signer = await ethers.provider.getSigner(address);
@@ -49,7 +32,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   }
 
   for (const symbol of Object.keys(tokensForNetwork)) {
-    const { name, address } = tokensForNetwork[symbol];
+    const { name, address, whale, collateralRate, oracleName, decimals } =
+      tokensForNetwork[symbol];
     console.log("------");
     console.log(
       "Exafin for %s will use: %s",
@@ -60,8 +44,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     const exafin = await hre.deployments.deploy("Exafin", {
       from: deployer,
-      args: [address, nameForOracle.get(symbol), exaFront.address],
-      log: true,
+      args: [address, oracleName, exaFront.address],
+      log: true
     });
 
     // We transfer ownership of Exafin to ExaFront
@@ -78,24 +62,32 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       { from: deployer },
       "enableMarket",
       exafin.address,
-      tokensCollateralRate.get(symbol),
+      parseUnits(collateralRate, 18),
       symbol,
       name
     );
 
     console.log("Exafin %s deployed to: %s", symbol, exafin.address);
 
-    let dai = await ethers.getContractAt(
-      "IERC20",
-      "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-    );
-    const whaleSigner = await impersonate();
+    if (!process.env.PUBLIC_ADDRESS) {
+      console.log("Add PUBLIC_ADDRESS key to your .env file");
+    } else {
+      if (whale) {
+        let contract = await ethers.getContractAt("IERC20", address);
 
-    dai = dai.connect(whaleSigner);
-    await dai.transfer(
-      "0xd1Cd4c2e15Bf0D05796c7C9f7c0Eaba30119f454",
-      ethers.utils.parseEther("100")
-    );
+        const whaleSigner = await impersonate(whale);
+        contract = contract.connect(whaleSigner);
+
+        await contract.transfer(
+          process.env.PUBLIC_ADDRESS,
+          ethers.utils.parseUnits("100", decimals)
+        );
+
+        console.log(`Added 100 ${symbol} to ${process.env.PUBLIC_ADDRESS}`);
+      } else {
+        console.log(`There is no whale added for ${symbol}`);
+      }
+    }
   }
 };
 
