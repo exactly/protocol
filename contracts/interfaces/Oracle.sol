2f22b242be4dd7eac626a69498c1426a59d4a7ba
Lucas <lucas.lain@gmail.com>
2021-09-26 14:12:53 +0200

    Removed comment

--- contracts/interfaces/Oracle.sol
+++ contracts/interfaces/Oracle.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 interface Oracle {
-    function price(string memory symbol) external view returns (uint); // use 0x6d2299c48a8dd07a872fdd0f8233924872ad1071
+    function price(string memory symbol) external view returns (uint);
 }
