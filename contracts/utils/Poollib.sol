bb0b6e13a63c71ad4fc1a47abe8601381fa79eab
Lucas <lucas.lain@gmail.com>
2021-10-07 09:32:31 +0200

    Unused lib

--- contracts/utils/Poollib.sol
+++ contracts/utils/Poollib.sol
@@ -11,31 +11,4 @@ library PoolLib {
         uint256 supplied;
     }
 
-    struct AssetSource {
-        uint256 fromPool;
-        uint256 fromPot;
-    }
-
-    function borrowAdd(Pool calldata pool, uint256 amountBorrow) internal pure returns (AssetSource memory) {
-        AssetSource memory assetSource;
-
-        uint256 newBorrow = pool.borrowed + amountBorrow;
-
-        // if the new borrow is still under the supplied money
-        if (newBorrow <= pool.supplied) {
-            assetSource.fromPool = amountBorrow;
-        
-        // if the previous borrowed amount was already bigger than the supplied money
-        } else if (pool.borrowed >= pool.supplied) {
-            assetSource.fromPot = amountBorrow;
-
-        // if the amount of borrowed money needs to be taken a bit from the pool
-        // and the rest from the pot
-        } else {
-            assetSource.fromPool = pool.supplied - pool.borrowed;
-            assetSource.fromPot = amountBorrow - assetSource.fromPool;
-        }
-
-        return assetSource;
-    }
 }
