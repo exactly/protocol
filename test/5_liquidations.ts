b922906fd51e80f826d4625c33f38a62b96cf912
capu <hi@capu.tech>
2021-11-12 09:57:11 -0300

    test: a position can be re-collateralized through liquidation
    Also refactored the tests a little for extra clarity (the concept of
    collateralization ratio makes less sense here than in UMA, what I was
    familiar with)
    
    Added some assertions in existing testcases for the amounts & events of
    collateral seize

--- test/5_liquidations.ts
+++ test/5_liquidations.ts
@@ -77,7 +77,7 @@ describe("Liquidations", function () {
     await dai.transfer(bob.address, parseUnits("100000"));
   });
 
-  describe("GIVEN alice supplies USD63k worth of WBTC, USD3k worth of ETH (66k total) AND bob supplies 65kDAI", () => {
+  describe("GIVEN alice supplies USD63k worth of WBTC, USD3k worth of ETH (66k total), 63k*0.6+3k*0.7=39k liquidity AND bob supplies 65kDAI", () => {
     beforeEach(async () => {
       // we supply Eth to the protocol
       const amountETH = parseUnits("1");
@@ -93,9 +93,10 @@ describe("Liquidations", function () {
       const amountDAI = parseUnits("65000");
       await dai.connect(bob).approve(exafinDAI.address, amountDAI);
       await exafinDAI.connect(bob).supply(bob.address, amountDAI, nextPoolID);
+      await dai.connect(bob).approve(exafinDAI.address, parseUnits("100000"));
     });
 
-    describe("AND GIVEN Alice takes the biggest loan she can (39850 DAI), collaterallization 1.65", () => {
+    describe("AND GIVEN Alice takes the biggest loan she can (39850 DAI), 50 buffer for interest", () => {
       beforeEach(async () => {
         // we make ETH & WBTC count as collateral
         await auditor.enterMarkets(
@@ -109,34 +110,81 @@ describe("Liquidations", function () {
         await exafinDAI.borrow(amountToBorrowDAI, nextPoolID);
       });
 
-      describe("GIVEN a sufficient allowance on the liquidator", () => {
+      describe("WHEN the pool matures (prices stay the same)", () => {
         beforeEach(async () => {
-          await dai
-            .connect(bob)
-            .approve(exafinDAI.address, parseUnits("40000"));
+          await ethers.provider.send("evm_setNextBlockTimestamp", [nextPoolID]);
+          await ethers.provider.send("evm_mine", []);
         });
-        describe("WHEN the pool matures (prices stay the same)", () => {
+        it("THEN the position is liquidateable", async () => {
+          const tx = exafinDAI
+            .connect(bob)
+            .liquidate(
+              alice.address,
+              parseUnits("15000"),
+              exafinWBTC.address,
+              nextPoolID
+            );
+          await expect(tx).to.not.be.reverted;
+        });
+      });
+      describe("A position can be recollateralized through liquidation", () => {
+        describe("AND WHEN ETH price halves (Alices liquidity is 63k*0.6+1.5k*0.7=38850)", () => {
           beforeEach(async () => {
-            await ethers.provider.send("evm_setNextBlockTimestamp", [
-              nextPoolID,
-            ]);
-            await ethers.provider.send("evm_mine", []);
+            await exactlyEnv.setOracleMockPrice("ETH", "1500");
           });
-          it("THEN the position is liquidateable", async () => {
-            const tx = exafinDAI
-              .connect(bob)
-              .liquidate(
-                alice.address,
-                parseUnits("15000"),
-                exafinWBTC.address,
-                nextPoolID
-              );
-            await expect(tx).to.not.be.reverted;
+          it("THEN alice has a small (1k) liquidity shortfall", async () => {
+            let shortfall = (
+              await auditor.getAccountLiquidity(alice.address, nextPoolID)
+            )[1];
+            expect(shortfall).to.be.gt(parseUnits("1000"));
+            expect(shortfall).to.be.lt(parseUnits("1100"));
+          });
+          // The liquidator has an incentive to repay as much of the debt as
+          // possible (assuming he has an incentive to repay the debt in the
+          // first place, see below), since _as soon as the position is
+          // undercollateralized_, liquidators can repay half of its debt,
+          // regardless of the user's shortfall
+          describe("AND WHEN a liquidator repays the max amount (19kDAI)", () => {
+            let tx: any;
+            beforeEach(async () => {
+              tx = await exafinDAI
+                .connect(bob)
+                .liquidate(
+                  alice.address,
+                  parseUnits("19000"),
+                  exafinWBTC.address,
+                  nextPoolID
+                );
+            });
+            it("THEN alice no longer has a liquidity shortfall", async () => {
+              const shortfall = (
+                await auditor.getAccountLiquidity(alice.address, nextPoolID)
+              )[1];
+              expect(shortfall).to.eq(0);
+            });
+            it("AND the liquidator seized 19k of collateral (WBTC)", async () => {
+              // FIXME the liquidator should get a price better than market
+              // 19kusd of btc at its current price of 63kusd
+              const seizedWBTC = parseUnits("30158730", 0);
+              await expect(tx)
+                .to.emit(exafinWBTC, "Seized")
+                .withArgs(bob.address, alice.address, seizedWBTC, nextPoolID);
+            });
+            // debt: 39850-19000 = 20850
+            // liquidity: (1-.30158730)*0.6*63000+1500*0.7 = 27450
+            // 27450- 20850= 6600
+            it("AND she has some liquidity", async () => {
+              const liquidity = (
+                await auditor.getAccountLiquidity(alice.address, nextPoolID)
+              )[0];
+              expect(liquidity).to.be.gt(parseUnits("6500"));
+              expect(liquidity).to.be.lt(parseUnits("6600"));
+            });
           });
         });
       });
 
-      describe("AND GIVEN ETH price halves (Alices collateral goes from 66k to 38.5k), collaterallization: 0.96", () => {
+      describe("AND WHEN WBTC price halves (Alices liquidity is 32.5k*0.6+3k*0.7=21.6k)", () => {
         beforeEach(async () => {
           await exactlyEnv.setOracleMockPrice("WBTC", "32500");
         });
@@ -148,11 +196,11 @@ describe("Liquidations", function () {
           )[0];
           expect(liquidityAfterOracleChange).to.be.lt("1");
         });
-        it("AND alice has a liquidity shortfall", async () => {
+        it("AND alice has a big (18k) liquidity shortfall", async () => {
           let shortfall = (
             await auditor.getAccountLiquidity(alice.address, nextPoolID)
           )[1];
-          expect(shortfall).to.be.gt(0);
+          expect(shortfall).to.be.gt(parseUnits("18000"));
         });
         it("AND trying to repay an amount of zero fails", async () => {
           // We try to get all the ETH we can
@@ -197,12 +245,7 @@ describe("Liquidations", function () {
           });
         });
 
-        describe("GIVEN a sufficient allowance on the liquidator", () => {
-          beforeEach(async () => {
-            await dai
-              .connect(bob)
-              .approve(exafinDAI.address, parseUnits("100000"));
-          });
+        describe("Liquidation error cases", () => {
           it("WHEN trying to liquidate 39850 DAI for ETH (of which there is only 3000usd), THEN it reverts with a TOKENS_MORE_THAN_BALANCE error", async () => {
             // We expect liquidation to fail because trying to liquidate
             // and take over a collateral that bob doesn't have enough
@@ -232,49 +275,51 @@ describe("Liquidations", function () {
                 )
             ).to.be.revertedWith(errorGeneric(ProtocolError.TOO_MUCH_REPAY));
           });
-          describe("AND WHEN liquidating slightly less than the close factor (19000 DAI)", () => {
-            let tx: any;
-            beforeEach(async () => {
-              tx = exafinDAI
-                .connect(bob)
-                .liquidate(
-                  alice.address,
-                  parseUnits("19000"),
-                  exafinWBTC.address,
-                  nextPoolID
-                );
-              await tx;
-            });
-            // FIXME: this shouldn't work like this, the liquidation should be
-            // at a slightly better than market price
-            it("THEN roughly 19000 USD of collateral (WBTC) is seized", async () => {
-              const protocolShare = parseUnits("0.028");
-              // this is equivalent to 18999.9 USD, at the provided price of 32500
-              const seizedWBTC = parseUnits("58461538", 0);
-              await expect(tx)
-                .to.emit(exafinWBTC, "Seized")
-                .withArgs(bob.address, alice.address, seizedWBTC, nextPoolID);
-              const fee = seizedWBTC.mul(protocolShare).div(parseUnits("1"));
-              // note that the liquidator is actually being paid 2.8% less (!!)
-              // than market rate
-              expect(await wbtc.balanceOf(bob.address)).to.eq(
-                seizedWBTC.sub(fee)
+        });
+        // TODO: I think this should eventually be 'a position can be wiped out
+        // if its undercollateralized enough' kind of testsuite
+        describe("AND WHEN liquidating slightly less than the close factor (19000 DAI)", () => {
+          let tx: any;
+          beforeEach(async () => {
+            tx = exafinDAI
+              .connect(bob)
+              .liquidate(
+                alice.address,
+                parseUnits("19000"),
+                exafinWBTC.address,
+                nextPoolID
               );
-            });
-            it("AND 19000 DAI of debt is repaid", async () => {
-              const bobDAIBalanceBefore = parseUnits("35000");
-              await expect(tx)
-                .to.emit(exafinDAI, "Repaid")
-                .withArgs(
-                  bob.address,
-                  alice.address,
-                  parseUnits("19000"),
-                  nextPoolID
-                );
-              expect(await dai.balanceOf(bob.address)).to.eq(
-                bobDAIBalanceBefore.sub(parseUnits("19000"))
+            await tx;
+          });
+          // FIXME: this shouldn't work like this, the liquidation should be
+          // at a slightly better than market price
+          it("THEN roughly 19000 USD of collateral (WBTC) is seized", async () => {
+            const protocolShare = parseUnits("0.028");
+            // this is equivalent to 18999.9 USD, at the provided price of 32500
+            const seizedWBTC = parseUnits("58461538", 0);
+            await expect(tx)
+              .to.emit(exafinWBTC, "Seized")
+              .withArgs(bob.address, alice.address, seizedWBTC, nextPoolID);
+            const fee = seizedWBTC.mul(protocolShare).div(parseUnits("1"));
+            // note that the liquidator is actually being paid 2.8% less (!!)
+            // than market rate
+            expect(await wbtc.balanceOf(bob.address)).to.eq(
+              seizedWBTC.sub(fee)
+            );
+          });
+          it("AND 19000 DAI of debt is repaid", async () => {
+            const bobDAIBalanceBefore = parseUnits("35000");
+            await expect(tx)
+              .to.emit(exafinDAI, "Repaid")
+              .withArgs(
+                bob.address,
+                alice.address,
+                parseUnits("19000"),
+                nextPoolID
               );
-            });
+            expect(await dai.balanceOf(bob.address)).to.eq(
+              bobDAIBalanceBefore.sub(parseUnits("19000"))
+            );
           });
         });
       });
