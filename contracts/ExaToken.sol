6d1a44ca3eda6a24286034bafa0fdfacf1780277
Santiago <santisanchezavalos@gmail.com>
2021-11-15 12:44:56 -0300

    Removed TEAM_ROLE from contracts that extend from AccessControl

--- contracts/ExaToken.sol
+++ contracts/ExaToken.sol
@@ -7,11 +7,9 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract ExaToken is ERC20, ERC20Snapshot, AccessControl, ERC20Permit {
-    bytes32 public constant TEAM_ROLE = keccak256("TEAM_ROLE");
 
     constructor() ERC20("ExaToken", "EXA") ERC20Permit("ExaToken") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(TEAM_ROLE, msg.sender);
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
