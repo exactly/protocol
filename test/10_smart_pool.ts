53eb4700bd5f3e5ec6be0632856aa5a3fb366b42
Lucas <lucas.lain@gmail.com>
2021-11-19 21:25:19 +0100

    Fixed Tests

--- test/10_smart_pool.ts
+++ test/10_smart_pool.ts
@@ -18,6 +18,8 @@ describe("Smart Pool", function () {
   let eDAI: Contract;
   let bob: SignerWithAddress;
   let john: SignerWithAddress;
+  let bobBalancePre = parseUnits("2000");
+  let johnBalancePre = parseUnits("2000");
 
   const mockedTokens = new Map([
     [
@@ -31,7 +33,7 @@ describe("Smart Pool", function () {
   ]);
 
   beforeEach(async () => {
-    [bob, john] = await ethers.getSigners();
+    [, bob, john] = await ethers.getSigners();
 
     exactlyEnv = await ExactlyEnv.create(mockedTokens);
     eDAI = exactlyEnv.getEToken("DAI");
@@ -39,24 +41,23 @@ describe("Smart Pool", function () {
     underlyingToken = exactlyEnv.getUnderlying("DAI");
     fixedLender = exactlyEnv.getFixedLender("DAI");
     await eDAI.setFixedLender(fixedLender.address);
-
     // From Owner to User
-    await underlyingToken.transfer(bob.address, parseUnits("2000"));
-    await underlyingToken.transfer(john.address, parseUnits("2000"));
+    await underlyingToken.transfer(bob.address, bobBalancePre);
+    await underlyingToken.transfer(john.address, johnBalancePre);
   });
 
   describe("GIVEN bob has 2000DAI in balance, AND deposits 1000DAI", () => {
     beforeEach(async () => {
-      let bobBalance = parseUnits("2000");
-      let johnBalance = parseUnits("2000");
       const fixedLenderJohn = fixedLender.connect(john);
-      await underlyingToken.approve(fixedLender.address, bobBalance);
+      const fixedLenderBob = fixedLender.connect(bob);
+      const underlyingTokenBob = underlyingToken.connect(bob);
+      const underlyingTokenJohn = underlyingToken.connect(john);
 
-      const underlyingTokenUser = underlyingToken.connect(john);
-      await underlyingTokenUser.approve(fixedLender.address, johnBalance);
+      await underlyingTokenBob.approve(fixedLender.address, bobBalancePre);
+      await underlyingTokenJohn.approve(fixedLender.address, johnBalancePre);
 
       await fixedLenderJohn.depositToSmartPool(parseUnits("1000"));
-      await fixedLender.depositToSmartPool(parseUnits("1000"));
+      await fixedLenderBob.depositToSmartPool(parseUnits("1000"));
     });
     it("THEN balance of DAI in contract is 2000", async () => {
       let balanceOfAssetInContract = await underlyingToken.balanceOf(
@@ -71,15 +72,14 @@ describe("Smart Pool", function () {
       expect(balanceOfETokenInUserAddress).to.equal(parseUnits("1000"));
     });
     it("AND WHEN bob deposits 100DAI more, THEN event DepositToSmartPool is emitted", async () => {
-      await expect(fixedLender.depositToSmartPool(parseUnits("100"))).to.emit(
-        fixedLender,
-        "DepositToSmartPool"
-      );
+      await expect(
+        fixedLender.connect(bob).depositToSmartPool(parseUnits("100"))
+      ).to.emit(fixedLender, "DepositToSmartPool");
     });
     describe("AND bob withdraws 500DAI", () => {
       beforeEach(async () => {
         let amountToWithdraw = parseUnits("500");
-        await fixedLender.withdrawFromSmartPool(amountToWithdraw);
+        await fixedLender.connect(bob).withdrawFromSmartPool(amountToWithdraw);
       });
       it("THEN balance of DAI in contract is 1500", async () => {
         let balanceOfAssetInContract = await underlyingToken.balanceOf(
@@ -95,12 +95,12 @@ describe("Smart Pool", function () {
       });
       it("AND WHEN bob withdraws 100DAI more, THEN event WithdrawFromSmartPool is emitted", async () => {
         await expect(
-          fixedLender.withdrawFromSmartPool(parseUnits("100"))
+          fixedLender.connect(bob).withdrawFromSmartPool(parseUnits("100"))
         ).to.emit(fixedLender, "WithdrawFromSmartPool");
       });
       it("AND WHEN bob wants to withdraw 600DAI more, THEN it reverts because his eDAI balance is not enough", async () => {
         await expect(
-          fixedLender.withdrawFromSmartPool(parseUnits("600"))
+          fixedLender.connect(bob).withdrawFromSmartPool(parseUnits("600"))
         ).to.be.revertedWith(
           errorGeneric(ProtocolError.BURN_AMOUNT_EXCEEDS_BALANCE)
         );
@@ -108,8 +108,12 @@ describe("Smart Pool", function () {
 
       it("AND WHEN bob wants to withdraw all the assets, THEN he doesn't need to especifically set the amount", async () => {
         await expect(
-          fixedLender.withdrawFromSmartPool(ethers.constants.MaxUint256)
+          fixedLender
+            .connect(bob)
+            .withdrawFromSmartPool(ethers.constants.MaxUint256)
         ).to.not.be.reverted;
+        const bobBalancePost = await underlyingToken.balanceOf(bob.address);
+        expect(bobBalancePre).to.equal(bobBalancePost);
       });
     });
   });
