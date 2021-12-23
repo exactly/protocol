a46d4e5320d1f3108e4c199aad4a5d5b97dfe7e5
Santiago <santisanchezavalos@gmail.com>
2021-12-23 01:11:57 -0300

    Refactored pausable tests and added new depositSp function in default env

--- test/15_pausable.ts
+++ test/15_pausable.ts
@@ -8,7 +8,6 @@ import { DefaultEnv } from "./defaultEnv";
 describe("FixedLender - Pausable", function () {
   let exactlyEnv: DefaultEnv;
 
-  let underlyingToken: Contract;
   let fixedLender: Contract;
 
   let owner: SignerWithAddress;
@@ -23,26 +22,32 @@ describe("FixedLender - Pausable", function () {
       [owner, user] = await ethers.getSigners();
 
       exactlyEnv = await ExactlyEnv.create({});
-
-      underlyingToken = exactlyEnv.getUnderlying("DAI");
       fixedLender = exactlyEnv.getFixedLender("DAI");
-
       PAUSER_ROLE = await fixedLender.PAUSER_ROLE();
+
       fixedLender.grantRole(PAUSER_ROLE, owner.address);
     });
-    it("AND GIVEN a pause called from third parties, THEN it should revert with AccessControl error", async () => {
+    it("AND WHEN a pause is called from third parties, THEN it should revert with AccessControl error", async () => {
       await expect(fixedLender.connect(user).pause()).to.be.revertedWith(
         "AccessControl"
       );
     });
-    it("AND GIVEN an unpause called from third parties, THEN it should revert with AccessControl error", async () => {
+    it("AND WHEN an unpause is called from third parties, THEN it should revert with AccessControl error", async () => {
       await expect(fixedLender.connect(user).unpause()).to.be.revertedWith(
         "AccessControl"
       );
     });
-    it("AND GIVEN a grant in the PAUSER role to another user, THEN it should NOT revert when user pauses actions", async () => {
-      fixedLender.grantRole(PAUSER_ROLE, user.address);
-      await expect(fixedLender.connect(user).pause()).to.not.be.reverted;
+    describe("AND GIVEN a grant in the PAUSER role to another user", () => {
+      beforeEach(async () => {
+        fixedLender.grantRole(PAUSER_ROLE, user.address);
+      });
+      it("THEN it should NOT revert when user pauses actions", async () => {
+        await expect(fixedLender.connect(user).pause()).to.not.be.reverted;
+      });
+      it("THEN it should NOT revert when user unpauses actions", async () => {
+        fixedLender.connect(user).pause();
+        await expect(fixedLender.connect(user).unpause()).to.not.be.reverted;
+      });
     });
     describe("AND GIVEN a pause for all actions that have whenNotPaused modifier", () => {
       beforeEach(async () => {
@@ -91,9 +96,7 @@ describe("FixedLender - Pausable", function () {
           fixedLender.unpause();
         });
         it("THEN it should NOT revert when trying to call one of them", async () => {
-          await underlyingToken.approve(fixedLender.address, "100");
-          await expect(fixedLender.depositToSmartPool("100")).to.not.be
-            .reverted;
+          await expect(exactlyEnv.depositSP("DAI", "100")).to.not.be.reverted;
         });
       });
     });
