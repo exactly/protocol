05fa9c08817d52f600cfab6f1ef8644cefeac9d5
Lucas <lucas.lain@gmail.com>
2022-01-08 13:18:27 +0100

    Removed debugging

--- contracts/FixedLender.sol
+++ contracts/FixedLender.sol
@@ -15,7 +15,6 @@ import "./interfaces/IInterestRateModel.sol";
 import "./utils/TSUtils.sol";
 import "./utils/DecimalMath.sol";
 import "./utils/Errors.sol";
-import "hardhat/console.sol";
 
 contract FixedLender is IFixedLender, ReentrancyGuard, AccessControl, Pausable {
     using SafeERC20 for IERC20;
