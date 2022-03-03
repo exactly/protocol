7440f93e1078ec2800493cd00523b03f1800ddf7
danilo neves cruz <cruzdanilo@gmail.com>
2022-03-03 10:44:31 -0300

    test: 3_oracle: use deploy fixture

--- test/3_oracle.ts
+++ test/3_oracle.ts
@@ -1,218 +1,107 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
-import { Contract } from "ethers";
-import { errorGeneric, ExaTime, ProtocolError } from "./exactlyUtils";
-import { parseUnits } from "ethers/lib/utils";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { DefaultEnv } from "./defaultEnv";
+import { ethers, deployments } from "hardhat";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import type { ExactlyOracle, MockedChainlinkFeedRegistry, MockedToken } from "../types";
+import GenericError, { ErrorCode } from "./utils/GenericError";
+import timelockExecute from "./utils/timelockExecute";
+import USD_ADDRESS from "./utils/USD_ADDRESS";
+
+const {
+  utils: { parseUnits },
+  getUnnamedSigners,
+  getNamedSigner,
+  getContract,
+  provider,
+} = ethers;
 
 describe("ExactlyOracle", function () {
-  let exactlyEnv: DefaultEnv;
-
-  let exactlyOracle: Contract;
-  let chainlinkFeedRegistry: Contract;
-  let underlyingToken: Contract;
+  let dai: MockedToken;
+  let feedRegistry: MockedChainlinkFeedRegistry;
+  let exactlyOracle: ExactlyOracle;
 
   let user: SignerWithAddress;
-  let snapshot: any;
-  let exaTime = new ExaTime();
-  let mockedDate = exaTime.timestamp;
-  let maxDelayTime: number;
-
-  // Set the MockedOracle prices to zero
-  let mockedTokens = new Map([
-    [
-      "DAI",
-      {
-        decimals: 18,
-        collateralRate: parseUnits("0.8"),
-        usdPrice: parseUnits("0"),
-      },
-    ],
-    [
-      "WETH",
-      {
-        decimals: 18,
-        collateralRate: parseUnits("0.7"),
-        usdPrice: parseUnits("0"),
-      },
-    ],
-  ]);
-  let chainlinkPrices = new Map([
-    ["DAI", { usdPrice: parseUnits("1", 8) }],
-    ["WETH", { usdPrice: parseUnits("3100", 8) }],
-  ]);
+  let owner: SignerWithAddress;
+  let timestamp: number;
 
   before(async () => {
-    [, user] = await ethers.getSigners();
-    exactlyEnv = await DefaultEnv.create({ mockedTokens });
-    underlyingToken = exactlyEnv.getUnderlying("DAI");
-    const ChainlinkFeedRegistryMock = await ethers.getContractFactory(
-      "MockedChainlinkFeedRegistry"
-    );
-    chainlinkFeedRegistry = await ChainlinkFeedRegistryMock.deploy();
-    await chainlinkFeedRegistry.deployed();
-
-    let tokenAddresses = new Array();
-    let tokenNames = new Array();
-    await Promise.all(
-      Array.from(chainlinkPrices.keys()).map(async (tokenName) => {
-        const token = exactlyEnv.getUnderlying(tokenName);
-        const { usdPrice } = chainlinkPrices.get(tokenName)!;
-        tokenAddresses.push(token.address);
-        tokenNames.push(tokenName);
-
-        await chainlinkFeedRegistry.setPrice(
-          token.address,
-          exactlyEnv.usdAddress,
-          usdPrice
-        );
-      })
-    );
-    await chainlinkFeedRegistry.setUpdatedAtTimestamp(exaTime.timestamp);
-
-    const ExactlyOracle = await ethers.getContractFactory("ExactlyOracle");
-    exactlyOracle = await ExactlyOracle.deploy(
-      chainlinkFeedRegistry.address,
-      tokenNames,
-      tokenAddresses,
-      exactlyEnv.usdAddress,
-      exactlyEnv.maxOracleDelayTime
-    );
-    await exactlyOracle.deployed();
-    await exactlyOracle.setAssetSources(tokenNames, tokenAddresses);
-    await exactlyEnv.setOracle(exactlyOracle.address);
-    maxDelayTime = await exactlyOracle.maxDelayTime();
-
-    // This helps with tests that use evm_setNextBlockTimestamp
-    snapshot = await ethers.provider.send("evm_snapshot", []);
+    owner = await getNamedSigner("multisig");
+    [user] = await getUnnamedSigners();
   });
 
   beforeEach(async () => {
-    mockedDate += exaTime.ONE_DAY; // we add a day so it's not the same timestamp than previous blocks
+    await deployments.fixture(["Markets"]);
+
+    dai = await getContract<MockedToken>("DAI", user);
+    feedRegistry = await getContract<MockedChainlinkFeedRegistry>("FeedRegistry", user);
+    exactlyOracle = await getContract<ExactlyOracle>("ExactlyOracle", user);
+
+    await dai.connect(owner).transfer(user.address, parseUnits("100000"));
+    timestamp = Math.floor(Date.now() / 1_000) + 1_000;
   });
 
   it("GetAssetPrice returns a positive and valid price value", async () => {
-    let priceOfEth = await exactlyOracle.getAssetPrice("WETH");
-    let priceOfDai = await exactlyOracle.getAssetPrice("DAI");
-
     // The price returned by the oracle is previously scaled to an 18-digit decimal
-    expect(priceOfEth).to.be.equal(
-      chainlinkPrices.get("WETH")!.usdPrice.mul(1e10)
-    );
-    expect(priceOfDai).to.be.equal(
-      chainlinkPrices.get("DAI")!.usdPrice.mul(1e10)
-    );
+    expect(await exactlyOracle.getAssetPrice("DAI")).to.equal(10n ** 18n);
+    expect(await exactlyOracle.getAssetPrice("WETH")).to.equal(1_000n * 10n ** 18n);
   });
 
   it("GetAssetPrice does not fail when Chainlink price is not older than maxDelayTime", async () => {
-    await chainlinkFeedRegistry.setUpdatedAtTimestamp(
-      mockedDate - (maxDelayTime - 1)
-    );
-
-    await exactlyEnv.moveInTime(mockedDate);
-    await ethers.provider.send("evm_mine", []);
-
+    await feedRegistry.setUpdatedAtTimestamp(timestamp - (86_400 - 1));
+    await provider.send("evm_setNextBlockTimestamp", [timestamp]);
+    await provider.send("evm_mine", []);
     await expect(exactlyOracle.getAssetPrice("DAI")).to.not.be.reverted;
   });
 
   it("GetAssetPrice does not fail when Chainlink price is equal to maxDelayTime", async () => {
-    await chainlinkFeedRegistry.setUpdatedAtTimestamp(
-      mockedDate - maxDelayTime
-    );
-
-    await exactlyEnv.moveInTime(mockedDate);
-    await ethers.provider.send("evm_mine", []);
-
+    await feedRegistry.setUpdatedAtTimestamp(timestamp - 86_400);
+    await provider.send("evm_setNextBlockTimestamp", [timestamp]);
+    await provider.send("evm_mine", []);
     await expect(exactlyOracle.getAssetPrice("DAI")).to.not.be.reverted;
   });
 
   it("GetAssetPrice should fail when Chainlink price is older than maxDelayTime", async () => {
-    await chainlinkFeedRegistry.setUpdatedAtTimestamp(
-      mockedDate - (maxDelayTime + 1)
-    );
-
-    await exactlyEnv.moveInTime(mockedDate);
-    await ethers.provider.send("evm_mine", []);
-
-    await expect(exactlyOracle.getAssetPrice("DAI")).to.be.revertedWith(
-      errorGeneric(ProtocolError.PRICE_ERROR)
-    );
+    await feedRegistry.setUpdatedAtTimestamp(timestamp - (86_400 + 1));
+    await provider.send("evm_setNextBlockTimestamp", [timestamp]);
+    await provider.send("evm_mine", []);
+    await expect(exactlyOracle.getAssetPrice("DAI")).to.be.revertedWith(GenericError(ErrorCode.PRICE_ERROR));
   });
 
   it("GetAssetPrice should fail when price value is zero", async () => {
-    await chainlinkFeedRegistry.setPrice(
-      underlyingToken.address,
-      exactlyEnv.usdAddress,
-      0
-    );
-
-    await expect(exactlyOracle.getAssetPrice("DAI")).to.be.revertedWith(
-      errorGeneric(ProtocolError.PRICE_ERROR)
-    );
+    await feedRegistry.setPrice(dai.address, USD_ADDRESS, 0);
+    await expect(exactlyOracle.getAssetPrice("DAI")).to.be.revertedWith(GenericError(ErrorCode.PRICE_ERROR));
   });
 
   it("GetAssetPrice should fail when price value is lower than zero", async () => {
-    await chainlinkFeedRegistry.setPrice(
-      underlyingToken.address,
-      exactlyEnv.usdAddress,
-      -10
-    );
-
-    await expect(exactlyOracle.getAssetPrice("DAI")).to.be.revertedWith(
-      errorGeneric(ProtocolError.PRICE_ERROR)
-    );
+    await feedRegistry.setPrice(dai.address, USD_ADDRESS, -10);
+    await expect(exactlyOracle.getAssetPrice("DAI")).to.be.revertedWith(GenericError(ErrorCode.PRICE_ERROR));
   });
 
   it("GetAssetPrice should fail when asset symbol is invalid", async () => {
-    await expect(exactlyOracle.getAssetPrice("INVALID")).to.be.revertedWith(
-      errorGeneric(ProtocolError.PRICE_ERROR)
-    );
+    await expect(exactlyOracle.getAssetPrice("INVALID")).to.be.revertedWith(GenericError(ErrorCode.PRICE_ERROR));
   });
 
   it("SetAssetSources should set the address source of an asset", async () => {
-    let linkSymbol = "LINK";
-    let linkAddress = "0x514910771AF9Ca656af840dff83E8264EcF986CA";
-
-    await expect(
-      await exactlyOracle.setAssetSources([linkSymbol], [linkAddress])
-    )
+    const linkSymbol = "LINK";
+    const linkAddress = "0x514910771AF9Ca656af840dff83E8264EcF986CA";
+    await timelockExecute(owner, exactlyOracle, "grantRole", [await exactlyOracle.DEFAULT_ADMIN_ROLE(), owner.address]);
+    await expect(await exactlyOracle.connect(owner).setAssetSources([linkSymbol], [linkAddress]))
       .to.emit(exactlyOracle, "SymbolSourceUpdated")
       .withArgs(linkSymbol, linkAddress);
-    await chainlinkFeedRegistry.setPrice(
-      linkAddress,
-      exactlyEnv.usdAddress,
-      10
-    );
-
-    await chainlinkFeedRegistry.setUpdatedAtTimestamp(
-      mockedDate - maxDelayTime
-    );
-    await exactlyEnv.moveInTime(mockedDate);
-    await ethers.provider.send("evm_mine", []);
-
-    await expect(exactlyOracle.getAssetPrice(linkSymbol)).to.not.be.reverted;
+    await feedRegistry.setPrice(linkAddress, USD_ADDRESS, 1);
+    await feedRegistry.setUpdatedAtTimestamp(timestamp - 86_400);
+    await provider.send("evm_setNextBlockTimestamp", [timestamp]);
+    await provider.send("evm_mine", []);
+    expect(await exactlyOracle.getAssetPrice(linkSymbol)).to.equal(1e10);
   });
 
   it("SetAssetSources should fail when called with different length for asset symbols and addresses", async () => {
+    await timelockExecute(owner, exactlyOracle, "grantRole", [await exactlyOracle.DEFAULT_ADMIN_ROLE(), owner.address]);
     await expect(
-      exactlyOracle.setAssetSources(
-        ["WETH", "BTC"],
-        ["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
-      )
-    ).to.be.revertedWith(
-      errorGeneric(ProtocolError.INCONSISTENT_PARAMS_LENGTH)
-    );
+      exactlyOracle.connect(owner).setAssetSources(["WETH", "BTC"], ["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]),
+    ).to.be.revertedWith(GenericError(ErrorCode.INCONSISTENT_PARAMS_LENGTH));
   });
 
   it("SetAssetSources should fail when called from third parties", async () => {
-    await expect(
-      exactlyOracle.connect(user).setAssetSources([], [])
-    ).to.be.revertedWith("AccessControl");
-  });
-
-  after(async () => {
-    await ethers.provider.send("evm_revert", [snapshot]);
-    await ethers.provider.send("evm_mine", []);
+    await expect(exactlyOracle.setAssetSources([], [])).to.be.revertedWith("AccessControl");
   });
 });
