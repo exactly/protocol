7198c029c759a45a1df2f88c9b912513ba366162
danilo neves cruz <cruzdanilo@gmail.com>
2022-03-09 19:27:37 -0300

    poollib: add basic unit tests for gas snapshots

---
+++ test/solidity/PoolLib.t.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import { Vm } from "forge-std/Vm.sol";
+import { DSTest } from "ds-test/test.sol";
+import { InsufficientProtocolLiquidity } from "../../contracts/utils/PoolLib.sol";
+import { GenericError, ErrorCode } from "../../contracts/utils/Errors.sol";
+import { PoolLib } from "../../contracts/utils/PoolLib.sol";
+
+contract PoolLibTest is DSTest {
+  using PoolLib for PoolLib.MaturityPool;
+
+  Vm internal vm = Vm(HEVM_ADDRESS);
+  PoolLib.MaturityPool private mp;
+
+  function testAtomicAddRemoveFee() external {
+    mp.addFee(1 ether);
+    assertEq(mp.borrowed, 0);
+    assertEq(mp.supplied, 0);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 1 ether);
+    assertEq(mp.lastAccrue, 0);
+
+    mp.removeFee(1 ether);
+    assertEq(mp.borrowed, 0);
+    assertEq(mp.supplied, 0);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 0);
+  }
+
+  function testAtomicDepositBorrowRepayWithdraw() external {
+    uint256 smartPoolDebtReduction = mp.depositMoney(1 ether);
+    assertEq(mp.borrowed, 0);
+    assertEq(mp.supplied, 1 ether);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 0);
+    assertEq(smartPoolDebtReduction, 0);
+
+    uint256 smartPoolDebt = mp.borrowMoney(1 ether, 0);
+    assertEq(mp.borrowed, 1 ether);
+    assertEq(mp.supplied, 1 ether);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 0);
+    assertEq(smartPoolDebt, 0);
+
+    smartPoolDebtReduction = mp.repayMoney(1 ether);
+    assertEq(mp.borrowed, 0);
+    assertEq(mp.supplied, 1 ether);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 0);
+    assertEq(smartPoolDebtReduction, 0);
+
+    smartPoolDebt = mp.withdrawMoney(1 ether, 0);
+    assertEq(mp.borrowed, 0);
+    assertEq(mp.supplied, 0);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 0);
+    assertEq(smartPoolDebt, 0);
+  }
+
+  function testSmartPoolBorrow() external {
+    uint256 smartPoolDebt = mp.borrowMoney(1 ether, 1 ether);
+    assertEq(mp.borrowed, 1 ether);
+    assertEq(mp.supplied, 0);
+    assertEq(mp.suppliedSP, 1 ether);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 0);
+    assertEq(smartPoolDebt, 1 ether);
+  }
+
+  function testEarningsAccrual() external {
+    mp.addFee(1 ether);
+    uint256 earnings = mp.accrueEarnings(1 days, 1 days);
+    assertEq(mp.borrowed, 0);
+    assertEq(mp.supplied, 0);
+    assertEq(mp.suppliedSP, 0);
+    assertEq(mp.earningsUnassigned, 0);
+    assertEq(mp.lastAccrue, 1 days);
+    assertEq(earnings, 1 ether);
+  }
+
+  function testEarningsDistribution() external {
+    (uint256 smartPool, uint256 treasury) = PoolLib.distributeEarningsAccordingly(2 ether, 1 ether, 2 ether);
+    assertEq(smartPool, 1 ether);
+    assertEq(treasury, 1 ether);
+  }
+
+  function testBorrowInsufficientLiquidity() external {
+    vm.expectRevert(InsufficientProtocolLiquidity.selector);
+    this.borrowMoney(1 ether, 1 ether - 1);
+  }
+
+  function borrowMoney(uint256 amount, uint256 maxDebt) public returns (uint256) {
+    return mp.borrowMoney(amount, maxDebt);
+  }
+}
