3d1dcd41f79ec0564228d4e6eef45636b384f0d1
Santiago Sanchez Avalos <santisanchezavalos@gmail.com>
2022-03-14 23:21:50 -0300

    add fixedLender solidity tests + gas snapshots

---
+++ test/solidity/FixedLender.t.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import { Vm } from "forge-std/Vm.sol";
+import { DSTest } from "ds-test/test.sol";
+import { FixedLender } from "../../contracts/FixedLender.sol";
+import { PoolAccounting } from "../../contracts/PoolAccounting.sol";
+import { InterestRateModel } from "../../contracts/InterestRateModel.sol";
+import { Auditor } from "../../contracts/Auditor.sol";
+import { EToken } from "../../contracts/EToken.sol";
+import { MockedToken } from "../../contracts/external/MockedToken.sol";
+import { MockedOracle } from "../../contracts/external/MockedOracle.sol";
+
+contract FixedLenderTest is DSTest {
+  Vm internal vm = Vm(HEVM_ADDRESS);
+  FixedLender internal fixedLender;
+  MockedToken internal mockedToken;
+
+  uint32 public constant INTERVAL = 7 days;
+  uint256 public nextMaturityDate;
+
+  event DepositToSmartPool(address indexed user, uint256 amount);
+  event WithdrawFromSmartPool(address indexed user, uint256 amount);
+  event DepositToMaturityPool(address indexed from, uint256 amount, uint256 fee, uint256 maturityDate);
+  event BorrowFromMaturityPool(address indexed to, uint256 amount, uint256 fee, uint256 maturityDate);
+  event WithdrawFromMaturityPool(address indexed from, uint256 amount, uint256 amountDiscounted, uint256 maturityDate);
+  event RepayToMaturityPool(
+    address indexed payer,
+    address indexed borrower,
+    uint256 repayAmount,
+    uint256 debtCovered,
+    uint256 maturityDate
+  );
+
+  function setUp() external {
+    mockedToken = new MockedToken("DAI", "DAI", 18, 100 ether);
+    MockedOracle mockedOracle = new MockedOracle();
+    mockedOracle.setPrice("DAI", 1e8);
+    Auditor auditor = new Auditor(address(mockedOracle));
+    EToken eToken = new EToken("DAI", "DAI", 18);
+    InterestRateModel interestRateModel = new InterestRateModel(0.0495e18, -0.025e18, 1.1e18, 0.00000023148e18, 0.1e18);
+    PoolAccounting poolAccounting = new PoolAccounting(address(interestRateModel));
+    fixedLender = new FixedLender(
+      address(mockedToken),
+      "DAI",
+      address(eToken),
+      address(auditor),
+      address(poolAccounting)
+    );
+    poolAccounting.initialize(address(fixedLender));
+    eToken.initialize(address(fixedLender), address(auditor));
+    auditor.enableMarket(fixedLender, 0.8e18, "DAI", "DAI", 18);
+    nextMaturityDate = block.timestamp - (block.timestamp % INTERVAL) + INTERVAL;
+
+    mockedToken.approve(address(fixedLender), 100 ether);
+  }
+
+  function testDepositToSmartPool() external {
+    vm.expectEmit(true, false, false, true);
+    emit DepositToSmartPool(address(this), 1 ether);
+
+    fixedLender.depositToSmartPool(1 ether);
+  }
+
+  function testWithdrawFromSmartPool() external {
+    fixedLender.depositToSmartPool(1 ether);
+    vm.roll(block.number + 1); // we increase block number to avoid same block deposit & withdraw error
+
+    vm.expectEmit(true, false, false, true);
+    emit WithdrawFromSmartPool(address(this), 1 ether);
+    fixedLender.withdrawFromSmartPool(1 ether);
+  }
+
+  function testDepositToMaturityPool() external {
+    vm.expectEmit(true, false, false, true);
+    emit DepositToMaturityPool(address(this), 1 ether, 0, nextMaturityDate);
+    fixedLender.depositToMaturityPool(1 ether, nextMaturityDate, 1 ether);
+  }
+
+  function testWithdrawFromMaturityPool() external {
+    fixedLender.depositToSmartPool(10 ether);
+    fixedLender.depositToMaturityPool(1 ether, nextMaturityDate, 1 ether);
+
+    vm.expectEmit(true, false, false, true);
+    emit WithdrawFromMaturityPool(address(this), 1 ether, 991549837178779841, nextMaturityDate);
+    fixedLender.withdrawFromMaturityPool(1 ether, 0.9 ether, nextMaturityDate);
+  }
+
+  function testBorrowFromMaturityPool() external {
+    fixedLender.depositToSmartPool(12 ether);
+
+    vm.expectEmit(true, false, false, true);
+    emit BorrowFromMaturityPool(address(this), 1 ether, 1819800394287518, nextMaturityDate);
+    fixedLender.borrowFromMaturityPool(1 ether, nextMaturityDate, 2 ether);
+  }
+
+  function testRepayToMaturityPool() external {
+    fixedLender.depositToSmartPool(12 ether);
+    fixedLender.borrowFromMaturityPool(1 ether, nextMaturityDate, 2 ether);
+
+    vm.expectEmit(true, false, false, true);
+    emit RepayToMaturityPool(address(this), address(this), 109013698630136986, 109013698630136986, nextMaturityDate);
+    fixedLender.repayToMaturityPool(address(this), nextMaturityDate, 109013698630136986, 109013698630136986);
+  }
+}
