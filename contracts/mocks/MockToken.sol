3d1cf5afbd3031b7ddeac190c02e541ca85bc58a
danilo neves cruz <cruzdanilo@gmail.com>
2022-03-31 13:44:51 -0300

    mocktoken: remove commission logic

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -5,8 +5,6 @@ import { ERC20 } from "@rari-capital/solmate/src/tokens/ERC20.sol";
 import { IFlashBorrower } from "./IFlashBorrower.sol";
 
 contract MockToken is ERC20 {
-  uint256 private transferCommission = 0;
-
   /// @dev Constructor that gives msg.sender all of existing tokens.
   constructor(
     string memory name_,
@@ -21,23 +19,5 @@ contract MockToken is ERC20 {
     _mint(msg.sender, amount);
     IFlashBorrower(msg.sender).doThingsWithFlashLoan();
     _burn(msg.sender, amount);
-  }
-
-  function setCommission(uint256 _transferCommission) public {
-    transferCommission = _transferCommission;
-  }
-
-  function transferFrom(
-    address sender,
-    address recipient,
-    uint256 amount
-  ) public override returns (bool) {
-    amount = ((amount * (1e18 - transferCommission)) / 1e18);
-    return super.transferFrom(sender, recipient, amount);
-  }
-
-  function transfer(address recipient, uint256 amount) public override returns (bool) {
-    amount = ((amount * (1e18 - transferCommission)) / 1e18);
-    return super.transfer(recipient, amount);
   }
 }
