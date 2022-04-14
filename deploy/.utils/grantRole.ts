27862224c5dc71ce7a0194d1d87ac3b9e1694f2d
danilo neves cruz <cruzdanilo@gmail.com>
2022-04-14 16:32:42 -0300

    deploy: output operation logs

--- deploy/.utils/grantRole.ts
+++ deploy/.utils/grantRole.ts
@@ -1,5 +1,8 @@
 import type { IAccessControl } from "../../types";
 
 export default async (contract: IAccessControl, role: string, account: string) => {
-  if (!(await contract.hasRole(role, account))) await contract.grantRole(role, account);
+  if (!(await contract.hasRole(role, account))) {
+    console.log("granting role", contract.address, role, account);
+    await contract.grantRole(role, account);
+  }
 };
