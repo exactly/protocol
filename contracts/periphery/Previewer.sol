74b4c1f89420a2512cdbe5293e1905d15005723e
Santiago Sanchez Avalos <santisanchezavalos@gmail.com>
2022-05-02 14:01:59 -0300

    previewer: add previewFeeAtMaturity

--- contracts/periphery/Previewer.sol
+++ contracts/periphery/Previewer.sol
@@ -26,8 +26,8 @@ contract Previewer {
 
     InterestRateModel interestRateModel = fixedLender.interestRateModel();
     PoolLib.MaturityPool memory pool;
-
     (pool.borrowed, pool.supplied, pool.earningsUnassigned, pool.lastAccrual) = fixedLender.maturityPools(maturity);
+
     uint256 unassignedEarnings = pool.earningsUnassigned -
       pool.earningsUnassigned.fmul(block.timestamp - pool.lastAccrual, maturity - pool.lastAccrual);
     uint256 borrowed = pool.borrowed;
@@ -35,5 +35,32 @@ contract Previewer {
     uint256 smartPoolBorrowed = borrowed - Math.min(borrowed, supplied);
 
     (earnings, ) = interestRateModel.getYieldForDeposit(smartPoolBorrowed, unassignedEarnings, amount);
+  }
+
+  /// @notice Gets the fee charged by a maturity when borrowing certain amount of assets.
+  /// @param fixedLender address of the market.
+  /// @param maturity maturity date/pool where the assets will be borrowed.
+  /// @param amount amount of assets that will be borrowed.
+  /// @return fees amount that the depositor will also repay at maturity.
+  function previewFeeAtMaturity(
+    FixedLender fixedLender,
+    uint256 maturity,
+    uint256 amount
+  ) external view returns (uint256 fees) {
+    InterestRateModel interestRateModel = fixedLender.interestRateModel();
+    PoolLib.MaturityPool memory pool;
+    (pool.borrowed, pool.supplied, , ) = fixedLender.maturityPools(maturity);
+
+    fees = amount.fmul(
+      interestRateModel.getRateToBorrow(
+        maturity,
+        block.timestamp,
+        amount,
+        pool.borrowed,
+        pool.supplied,
+        fixedLender.smartPoolBalance()
+      ),
+      1e18
+    );
   }
 }
