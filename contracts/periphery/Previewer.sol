067b9d571757a867542a426353687ba72035e901
Santiago Sanchez Avalos <santisanchezavalos@gmail.com>
2022-05-03 17:24:01 -0300

    previewer: add previewWithdrawBeforeMaturity

--- contracts/periphery/Previewer.sol
+++ contracts/periphery/Previewer.sol
@@ -17,27 +17,27 @@ contract Previewer {
   /// @param fixedLender address of the market.
   /// @param maturity maturity date/pool where the assets will be deposited.
   /// @param amount amount of assets that will be deposited.
-  /// @return earnings amount that the depositor will receive after maturity.
-  function previewYieldAtMaturity(
+  /// @return yield that the depositor will receive after maturity.
+  function previewDepositAtMaturity(
     FixedLender fixedLender,
     uint256 maturity,
     uint256 amount
-  ) external view returns (uint256 earnings) {
+  ) external view returns (uint256 yield) {
     if (block.timestamp > maturity) revert AlreadyMatured();
 
     PoolLib.MaturityPool memory pool;
     (pool.borrowed, pool.supplied, pool.earningsUnassigned, pool.lastAccrual) = fixedLender.maturityPools(maturity);
     (uint256 smartPoolBorrowed, uint256 unassignedEarnings) = getPoolData(fixedLender, maturity);
 
-    (earnings, ) = fixedLender.interestRateModel().getYieldForDeposit(smartPoolBorrowed, unassignedEarnings, amount);
+    (yield, ) = fixedLender.interestRateModel().getYieldForDeposit(smartPoolBorrowed, unassignedEarnings, amount);
   }
 
   /// @notice Gets the fee charged by a maturity when borrowing certain amount of assets.
   /// @param fixedLender address of the market.
   /// @param maturity maturity date/pool where the assets will be borrowed.
   /// @param amount amount of assets that will be borrowed.
-  /// @return fees amount that the depositor will also repay at maturity.
-  function previewFeeAtMaturity(
+  /// @return fees that the depositor will also repay at maturity.
+  function previewBorrowAtMaturity(
     FixedLender fixedLender,
     uint256 maturity,
     uint256 amount
@@ -58,12 +58,43 @@ contract Previewer {
     );
   }
 
+  /// @notice Gets the fee charged by a maturity when withdrawing certain amount of assets before maturity.
+  /// @param fixedLender address of the market.
+  /// @param maturity maturity date/pool where the assets will be withdrawn.
+  /// @param amount amount of assets that will be withdrawn.
+  /// @return fees that will be charged to the withdrawer.
+  function previewWithdrawAtMaturity(
+    FixedLender fixedLender,
+    uint256 maturity,
+    uint256 amount
+  ) external view returns (uint256 fees) {
+    if (block.timestamp >= maturity) revert AlreadyMatured();
+
+    PoolLib.MaturityPool memory pool;
+    (pool.borrowed, pool.supplied, , ) = fixedLender.maturityPools(maturity);
+
+    fees =
+      amount -
+      amount.fdiv(
+        1e18 +
+          fixedLender.interestRateModel().getRateToBorrow(
+            maturity,
+            block.timestamp,
+            amount,
+            pool.borrowed,
+            pool.supplied,
+            fixedLender.smartPoolBalance()
+          ),
+        1e18
+      );
+  }
+
   /// @notice Gets the discount offered by a maturity when repaying certain amount of assets before maturity.
   /// @param fixedLender address of the market.
   /// @param maturity maturity date/pool where the assets will be discounted when repaying.
   /// @param amount amount of assets that will be repayed.
-  /// @return discount amount that the repayer will receive.
-  function previewDiscountBeforeMaturity(
+  /// @return discount that the repayer will receive.
+  function previewRepayAtMaturity(
     FixedLender fixedLender,
     uint256 maturity,
     uint256 amount,
@@ -72,16 +103,14 @@ contract Previewer {
     if (block.timestamp >= maturity) revert AlreadyMatured();
 
     (uint256 smartPoolBorrowed, uint256 unassignedEarnings) = getPoolData(fixedLender, maturity);
-    PoolLib.Position memory position;
-    (position.principal, position.fee) = fixedLender.mpUserBorrowedAmount(maturity, borrower);
-    PoolLib.Position memory scaleDebtCovered = PoolLib.Position(position.principal, position.fee).scaleProportionally(
-      amount
-    );
+    PoolLib.Position memory debt;
+    (debt.principal, debt.fee) = fixedLender.mpUserBorrowedAmount(maturity, borrower);
+    PoolLib.Position memory coveredDebt = debt.scaleProportionally(amount);
 
     (discount, ) = fixedLender.interestRateModel().getYieldForDeposit(
       smartPoolBorrowed,
       unassignedEarnings,
-      scaleDebtCovered.principal
+      coveredDebt.principal
     );
   }
 
