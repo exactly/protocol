346b667c37013f160492648b4083ac2b0884e908
danilo neves cruz <cruzdanilo@gmail.com>
2022-05-24 13:39:00 -0300

    deploy: always set oracle prices when using mock

--- deploy/mocks/FeedRegistry.ts
+++ deploy/mocks/FeedRegistry.ts
@@ -5,25 +5,23 @@ import { USD_ADDRESS, ETH_ADDRESS, BTC_ADDRESS } from "../ExactlyOracle";
 const func: DeployFunction = async ({
   ethers: { getContract },
   network: { config },
-  deployments: { get, deploy },
+  deployments: { get, deploy, log },
   getNamedAccounts,
 }) => {
   const { deployer } = await getNamedAccounts();
-  const { newlyDeployed } = await deploy("FeedRegistry", {
+  const { receipt } = await deploy("FeedRegistry", {
     skipIfAlreadyDeployed: true,
     contract: "MockChainlinkFeedRegistry",
     from: deployer,
     log: true,
   });
-  if (newlyDeployed) {
+  if (receipt) {
     const feedRegistry = await getContract<MockChainlinkFeedRegistry>("FeedRegistry");
     for (const token of config.tokens) {
       const { address } = await get(token);
-      await feedRegistry.setPrice(
-        { WBTC: BTC_ADDRESS, WETH: ETH_ADDRESS }[token] ?? address,
-        USD_ADDRESS,
-        { WBTC: 63_000e8, WETH: 1_000e8 }[token] ?? 1e8,
-      );
+      const price = { WBTC: 63_000e8, WETH: 1_000e8 }[token] ?? 1e8;
+      log("setting price", token, price);
+      await feedRegistry.setPrice({ WBTC: BTC_ADDRESS, WETH: ETH_ADDRESS }[token] ?? address, USD_ADDRESS, price);
     }
   }
 };
