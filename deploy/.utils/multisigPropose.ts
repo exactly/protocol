c30790bfa96d4bd37adc1be5427ac9904b7b9d2c
danilo neves cruz <cruzdanilo@gmail.com>
2022-08-03 20:56:18 +0200

    🔨 multisig: always log proposals

--- deploy/.utils/multisigPropose.ts
+++ deploy/.utils/multisigPropose.ts
@@ -1,22 +1,23 @@
 import Safe from "@gnosis.pm/safe-core-sdk";
 import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
 import SafeServiceClient from "@gnosis.pm/safe-service-client";
-import { ethers, deployments, network, getNamedAccounts } from "hardhat";
+import { ethers, network, getNamedAccounts } from "hardhat";
 import type { Contract } from "ethers";
 import format from "./format";
 
-export default async (account: string, contract: Contract, functionName: string, args?: readonly unknown[]) => {
+export default async (account: string, contract: Contract, functionName: string, args: readonly unknown[] = []) => {
   const { [account]: senderAddress, multisig: safeAddress } = await getNamedAccounts();
   const ethAdapter = new EthersAdapter({ ethers, signer: await ethers.getSigner(senderAddress) });
   const safeService = new SafeServiceClient({ txServiceUrl: network.config.gnosisSafeTxService, ethAdapter });
-  const safeSdk = await Safe.create({ ethAdapter, safeAddress });
   const calldata = contract.interface.encodeFunctionData(functionName, args);
   if (
     !(await safeService.getPendingTransactions(safeAddress)).results.find(
       ({ to, data }) => to === contract.address && data === calldata,
     )
   ) {
-    deployments.log("multisig: proposing", `${await format(contract.address)}.${functionName}`, await format(args));
+    // eslint-disable-next-line no-console
+    console.log("multisig: proposing", `${await format(contract.address)}.${functionName}`, await format(args));
+    const safeSdk = await Safe.create({ ethAdapter, safeAddress });
     const safeTransaction = await safeSdk.createTransaction({ to: contract.address, value: "0", data: calldata });
     const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
     const senderSignature = await safeSdk.signTransactionHash(safeTxHash);
