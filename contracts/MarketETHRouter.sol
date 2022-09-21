b844d557abb7080bf5c1f156e738c8ed7a7e8985
danilo neves cruz <cruzdanilo@gmail.com>
2022-09-21 18:45:00 -0300

    ⚡️ eth-router: omit `msg.sender` as unwrap parameter

--- contracts/MarketETHRouter.sol
+++ contracts/MarketETHRouter.sol
@@ -18,9 +18,9 @@ contract MarketETHRouter is Initializable {
     _;
   }
 
-  modifier unwrap(uint256 assets, address receiver) {
+  modifier unwrap(uint256 assets) {
     _;
-    unwrapAndTransfer(assets, receiver);
+    unwrapAndTransfer(assets);
   }
 
   /// @custom:oz-upgrades-unsafe-allow constructor
@@ -43,24 +43,24 @@ contract MarketETHRouter is Initializable {
     shares = market.deposit(msg.value, msg.sender);
   }
 
-  function withdraw(uint256 assets) external unwrap(assets, msg.sender) returns (uint256 shares) {
+  function withdraw(uint256 assets) external unwrap(assets) returns (uint256 shares) {
     shares = market.withdraw(assets, address(this), msg.sender);
   }
 
-  function borrow(uint256 assets) external unwrap(assets, msg.sender) returns (uint256 borrowShares) {
+  function borrow(uint256 assets) external unwrap(assets) returns (uint256 borrowShares) {
     borrowShares = market.borrow(assets, address(this), msg.sender);
   }
 
   function repay(uint256 assets) external payable wrap returns (uint256 repaidAssets, uint256 borrowShares) {
     (repaidAssets, borrowShares) = market.repay(assets, msg.sender);
 
-    if (msg.value > repaidAssets) unwrapAndTransfer(msg.value - repaidAssets, msg.sender);
+    if (msg.value > repaidAssets) unwrapAndTransfer(msg.value - repaidAssets);
   }
 
   function refund(uint256 borrowShares) external payable wrap returns (uint256 repaidAssets) {
     repaidAssets = market.refund(borrowShares, msg.sender);
 
-    if (msg.value > repaidAssets) unwrapAndTransfer(msg.value - repaidAssets, msg.sender);
+    if (msg.value > repaidAssets) unwrapAndTransfer(msg.value - repaidAssets);
   }
 
   function depositAtMaturity(uint256 maturity, uint256 minAssetsRequired)
@@ -78,26 +78,26 @@ contract MarketETHRouter is Initializable {
     uint256 minAssetsRequired
   ) external returns (uint256 actualAssets) {
     actualAssets = market.withdrawAtMaturity(maturity, assets, minAssetsRequired, address(this), msg.sender);
-    unwrapAndTransfer(actualAssets, msg.sender);
+    unwrapAndTransfer(actualAssets);
   }
 
   function borrowAtMaturity(
     uint256 maturity,
     uint256 assets,
     uint256 maxAssetsAllowed
-  ) external unwrap(assets, msg.sender) returns (uint256 assetsOwed) {
+  ) external unwrap(assets) returns (uint256 assetsOwed) {
     return market.borrowAtMaturity(maturity, assets, maxAssetsAllowed, address(this), msg.sender);
   }
 
   function repayAtMaturity(uint256 maturity, uint256 assets) external payable wrap returns (uint256 repaidAssets) {
     repaidAssets = market.repayAtMaturity(maturity, assets, msg.value, msg.sender);
 
-    if (msg.value > repaidAssets) unwrapAndTransfer(msg.value - repaidAssets, msg.sender);
+    if (msg.value > repaidAssets) unwrapAndTransfer(msg.value - repaidAssets);
   }
 
-  function unwrapAndTransfer(uint256 assets, address receiver) internal {
+  function unwrapAndTransfer(uint256 assets) internal {
     weth.withdraw(assets);
-    receiver.safeTransferETH(assets);
+    msg.sender.safeTransferETH(assets);
   }
 }
 
