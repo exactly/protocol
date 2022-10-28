3d9db6e2b458f046762d6e63dba9a2e854551028
danilo neves cruz <cruzdanilo@gmail.com>
2022-10-28 18:56:38 -0300

    📄 set license

--- contracts/mocks/MockPriceFeed.sol
+++ contracts/mocks/MockPriceFeed.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
 import { MockERC20 } from "solmate/src/test/utils/mocks/MockERC20.sol";
