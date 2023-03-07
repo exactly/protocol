d8420792aa872a68b3b2715f0015e2e9f299feae
danilo neves cruz <cruzdanilo@gmail.com>
2023-03-07 15:51:35 -0700

    🔨 tenderly: verify contracts publicly and privately

--- deploy/.utils/tenderlify.ts
+++ deploy/.utils/tenderlify.ts
@@ -1,5 +1,5 @@
 import { basename } from "path";
-import { getChainId, network, tenderly } from "hardhat";
+import { config as hardhatConfig, getChainId, network, tenderly } from "hardhat";
 import type { Deployment } from "hardhat-deploy/types";
 
 export default async (name: string, deployment: Deployment) => {
@@ -20,25 +20,27 @@ export default async (name: string, deployment: Deployment) => {
 
   const chainId = await getChainId();
   const version = compiler.version.slice(0, compiler.version.indexOf("+"));
-
-  await tenderly.verifyAPI({
-    contracts: Object.entries(sources).map(([sourcePath, { content: source }]) => {
-      const contractName = basename(sourcePath, ".sol");
-      return {
-        contractName,
-        source,
-        sourcePath,
-        networks: { ...(contractName === name && { [chainId]: { address } }) },
-        compiler: { name: "solc", version },
-      };
-    }),
-    config: {
-      compiler_version: version,
-      optimizations_used: optimizer.enabled,
-      optimizations_count: optimizer.runs,
-      debug,
-    },
+  const contracts = Object.entries(sources).map(([sourcePath, { content: source }]) => {
+    const contractName = basename(sourcePath, ".sol");
+    return {
+      contractName,
+      source,
+      sourcePath,
+      networks: { ...(contractName === name && { [chainId]: { address } }) },
+      compiler: { name: "solc", version },
+    };
   });
+  const config = {
+    compiler_version: version,
+    optimizations_used: optimizer.enabled,
+    optimizations_count: optimizer.runs,
+    debug,
+  };
+  await Promise.all([
+    tenderly.verifyAPI({ contracts, config }),
+    // eslint-disable-next-line deprecation/deprecation
+    tenderly.pushAPI({ contracts, config }, hardhatConfig.tenderly.project, hardhatConfig.tenderly.username),
+  ]);
 
   return deployment;
 };
