aad7f3a372c9576521a05ce8e6f7f9a49bad9ebf
danilo neves cruz <cruzdanilo@gmail.com>
2023-07-21 21:25:30 +0200

    ⬆️ package: upgrade openzeppelin

--- scripts/Base.s.sol
+++ scripts/Base.s.sol
@@ -2,14 +2,14 @@
 pragma solidity 0.8.17;
 
 import { Script, stdJson, console2 as console } from "forge-std/Script.sol";
-import { ProxyAdmin, TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import { ProxyAdmin, ITransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 abstract contract BaseScript is Script {
   using stdJson for string;
 
   function upgrade(address proxy, address newImplementation) internal {
     vm.prank(deployment("ProxyAdmin"));
-    TransparentUpgradeableProxy(payable(proxy)).upgradeTo(newImplementation);
+    ITransparentUpgradeableProxy(payable(proxy)).upgradeTo(newImplementation);
   }
 
   function deployment(string memory name) internal returns (address addr) {
