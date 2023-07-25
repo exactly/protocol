fb2b39fbb3ed3ea00d18d5267b3684645bbc037e
danilo neves cruz <cruzdanilo@gmail.com>
2023-07-25 13:11:06 +0200

    📄 airdrop: use license compatible with etherscan

--- contracts/periphery/Airdrop.sol
+++ contracts/periphery/Airdrop.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.17;
 
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
