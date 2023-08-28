f1d85f15d62fe32839bbc4fd31ae4b0534dbe5ab
itofarina <farinalvaro@gmail.com>
2023-08-28 11:28:51 -0300

    🔨 deploy: update debt manager constructor arguments

--- deploy/DebtManager.ts
+++ deploy/DebtManager.ts
@@ -2,25 +2,18 @@ import type { DeployFunction } from "hardhat-deploy/types";
 import validateUpgrade from "./.utils/validateUpgrade";
 
 const func: DeployFunction = async ({ deployments: { deploy, get }, getNamedAccounts }) => {
-  const [
-    { address: auditor },
-    { address: permit2 },
-    { address: balancerVault },
-    { address: uniswapFactory },
-    { address: timelock },
-    { deployer },
-  ] = await Promise.all([
-    get("Auditor"),
-    get("Permit2"),
-    get("BalancerVault"),
-    get("UniswapV3Factory"),
-    get("TimelockController"),
-    getNamedAccounts(),
-  ]);
+  const [{ address: auditor }, { address: permit2 }, { address: balancerVault }, { address: timelock }, { deployer }] =
+    await Promise.all([
+      get("Auditor"),
+      get("Permit2"),
+      get("BalancerVault"),
+      get("TimelockController"),
+      getNamedAccounts(),
+    ]);
 
   await validateUpgrade(
     "DebtManager",
-    { args: [auditor, permit2, balancerVault, uniswapFactory], envKey: "DEBT_MANAGER" },
+    { args: [auditor, permit2, balancerVault], envKey: "DEBT_MANAGER" },
     async (name, opts) =>
       deploy(name, {
         ...opts,
@@ -39,6 +32,6 @@ const func: DeployFunction = async ({ deployments: { deploy, get }, getNamedAcco
 };
 
 func.tags = ["DebtManager"];
-func.dependencies = ["Governance", "Auditor", "Markets", "UniswapV3", "Balancer", "Permit2"];
+func.dependencies = ["Governance", "Auditor", "Markets", "Balancer", "Permit2"];
 
 export default func;
