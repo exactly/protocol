51aae9a8834a874cdbd3340448f707fc8ada64d9
itofarina <farinalvaro@gmail.com>
2023-09-18 16:41:42 -0300

    📝 esexa: add natspec

--- contracts/periphery/EscrowedEXA.sol
+++ contracts/periphery/EscrowedEXA.sol
@@ -9,18 +9,25 @@ import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/ac
 import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
 import { EXA } from "./EXA.sol";
 
+/// @title EscrowedEXA.
+/// @notice ERC20 token that can be used to escrow EXA and vest it linearly using Sablier.
 contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
   using SafeERC20Upgradeable for EXA;
   using FixedPointMathLib for uint128;
 
+  /// @notice Role that can redeem esEXA for EXA.
   bytes32 public constant REDEEMER_ROLE = keccak256("REDEEMER_ROLE");
+  /// @notice Role that can receive or transfer esEXA.
   bytes32 public constant TRANSFERRER_ROLE = keccak256("TRANSFERRER_ROLE");
   /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
   EXA public immutable exa;
   /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
   ISablierV2LockupLinear public immutable sablier;
+  /// @notice Ratio of EXA to reserve when vesting, represented with 18 decimals.
   uint256 public reserveRatio;
+  /// @notice Duration of vesting period.
   uint40 public vestingPeriod;
+  /// @notice Mapping of streamId to reserve amount.
   mapping(uint256 => uint256) public reserves;
 
   /// @custom:oz-upgrades-unsafe-allow constructor
@@ -31,6 +38,8 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     _disableInitializers();
   }
 
+  /// @notice Initializes the contract.
+  /// @dev can only be called once.
   function initialize(uint40 vestingPeriod_, uint256 reserveRatio_) external initializer {
     __ERC20_init("escrowed EXA", "esEXA");
     __ERC20Permit_init_unchained("escrowed EXA");
@@ -38,6 +47,7 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     __AccessControl_init_unchained();
 
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    /// @dev address(0) holds the TRANSFERRER_ROLE so the token can be minted or burnt.
     _grantRole(TRANSFERRER_ROLE, address(0));
 
     setVestingPeriod(vestingPeriod_);
@@ -45,23 +55,35 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     exa.safeApprove(address(sablier), type(uint256).max);
   }
 
+  /// @notice ERC20 transfer override to only allow transfers from/to TRANSFERRER_ROLE holders.
   function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
     if (!hasRole(TRANSFERRER_ROLE, from) && !hasRole(TRANSFERRER_ROLE, to)) revert Untransferable();
     super._beforeTokenTransfer(from, to, amount);
   }
 
+  /// @notice Mints esEXA for EXA.
+  /// @param amount Amount of EXA to mint.
+  /// @param to Address to send esEXA to.
   function mint(uint256 amount, address to) external {
     assert(amount != 0);
     exa.safeTransferFrom(msg.sender, address(this), amount);
     _mint(to, amount);
   }
 
+  /// @notice Redeems esEXA for EXA.
+  /// @param amount Amount of esEXA to redeem.
+  /// @param to Address to send EXA to.
+  /// @dev Caller must have REDEEMER_ROLE.
   function redeem(uint256 amount, address to) external onlyRole(REDEEMER_ROLE) {
     assert(amount != 0);
     _burn(msg.sender, amount);
     exa.safeTransfer(to, amount);
   }
 
+  /// @notice Starts a vesting stream.
+  /// @param amount Amount of EXA to vest.
+  /// @param to Address to vest to.
+  /// @return streamId of the vesting stream.
   function vest(uint128 amount, address to) public returns (uint256 streamId) {
     _burn(msg.sender, amount);
     uint256 reserve = amount.mulWadDown(reserveRatio);
@@ -81,11 +103,19 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     emit Vest(msg.sender, to, streamId, amount);
   }
 
+  /// @notice Starts a vesting stream using a permit.
+  /// @param amount Amount of EXA to vest.
+  /// @param to Address to vest to.
+  /// @param p Permit for the EXA reserve.
+  /// @return streamId of the vesting stream.
   function vest(uint128 amount, address to, Permit calldata p) external returns (uint256 streamId) {
     exa.safePermit(msg.sender, address(this), p.value, p.deadline, p.v, p.r, p.s);
     return vest(amount, to);
   }
 
+  /// @notice Cancels vesting streams.
+  /// @param streamIds Array of streamIds to cancel.
+  /// @return streamsReserves Amount of EXA in reserves that is returned to the cancelled stream holders.
   function cancel(uint256[] memory streamIds) external returns (uint256 streamsReserves) {
     uint256 balanceEXA = exa.balanceOf(address(this));
     streamsReserves = _cancel(streamIds);
@@ -93,6 +123,10 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     exa.safeTransfer(msg.sender, streamsReserves);
   }
 
+  /// @notice Cancels vesting streams and withdraws the remaining EXA.
+  /// @param streamIds Array of streamIds to cancel.
+  /// @return streamsReserves Amount of EXA in reserves that is returned to the cancelled streams holder.
+  /// @dev the caller must be the recepient of the streamIds.
   function _cancel(uint256[] memory streamIds) internal returns (uint256 streamsReserves) {
     for (uint256 i = 0; i < streamIds.length; ++i) {
       uint256 streamId = streamIds[i];
@@ -105,6 +139,8 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     emit Cancel(msg.sender, streamIds);
   }
 
+  /// @notice Withdraws the EXA from the vesting streamIds.
+  /// @param streamIds Array of streamIds to withdraw from.
   function withdrawMax(uint256[] memory streamIds) public {
     for (uint256 i = 0; i < streamIds.length; ++i) {
       uint256 streamId = streamIds[i];
@@ -113,15 +149,23 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     }
   }
 
+  /// @notice Withdraws the EXA from the vesting streamId.
+  /// @param streamId streamId to withdraw from.
   function withdrawMax(uint256 streamId) internal {
     if (sablier.withdrawableAmountOf(streamId) != 0) sablier.withdrawMax(streamId, msg.sender);
   }
 
+  /// @notice Sets the vesting period.
+  /// @param vestingPeriod_ New vesting period.
+  /// @dev Caller must have DEFAULT_ADMIN_ROLE.
   function setVestingPeriod(uint40 vestingPeriod_) public onlyRole(DEFAULT_ADMIN_ROLE) {
     vestingPeriod = vestingPeriod_;
     emit VestingPeriodSet(vestingPeriod_);
   }
 
+  /// @notice Sets the reserve ratio.
+  /// @param reserveRatio_ New reserve ratio.
+  /// @dev Caller must have DEFAULT_ADMIN_ROLE.
   function setReserveRatio(uint256 reserveRatio_) public onlyRole(DEFAULT_ADMIN_ROLE) {
     reserveRatio = reserveRatio_;
     emit ReserveRatioSet(reserveRatio_);
@@ -136,9 +180,13 @@ contract EscrowedEXA is ERC20VotesUpgradeable, AccessControlUpgradeable {
     return exa.CLOCK_MODE();
   }
 
+  /// @notice Event emitted when the reserve ratio is set.
   event ReserveRatioSet(uint256 reserveRatio);
+  /// @notice Event emitted when the vesting period is set.
   event VestingPeriodSet(uint256 vestingPeriod);
+  /// @notice Event emitted when vesting streams are cancelled.
   event Cancel(address indexed account, uint256[] streamIds);
+  /// @notice Event emitted when a vesting stream is created.
   event Vest(address indexed caller, address indexed account, uint256 indexed streamId, uint256 amount);
 }
 
