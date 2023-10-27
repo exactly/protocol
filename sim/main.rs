b2e599772873e100d3e5450250aaf5c2e96cb353
danilo neves cruz <cruzdanilo@gmail.com>
2023-10-27 18:20:57 -0300

    🧐 sim: deploy protocol

--- sim/main.rs
+++ sim/main.rs
@@ -1,10 +1,14 @@
 use anyhow::{Ok, Result};
 use arbiter_core::{environment::builder::EnvironmentBuilder, middleware::RevmMiddleware};
-use ethers::types::{Bytes, U256};
+use ethers::types::{Bytes, I256, U128, U256};
 
 use bindings::{
     auditor::{Auditor, LiquidationIncentive},
     erc1967_proxy::ERC1967Proxy,
+    interest_rate_model::InterestRateModel,
+    market::Market,
+    mock_erc20::MockERC20,
+    mock_price_feed::MockPriceFeed,
 };
 
 #[allow(unused_imports)]
@@ -13,12 +17,13 @@ mod bindings;
 #[tokio::main]
 pub async fn main() -> Result<()> {
     let environment = EnvironmentBuilder::new().build();
-    let client = RevmMiddleware::new(&environment, None)?;
+    let deployer = RevmMiddleware::new(&environment, None)?;
+
     let auditor = Auditor::new(
         ERC1967Proxy::deploy(
-            client.clone(),
+            deployer.clone(),
             (
-                Auditor::deploy(client.clone(), U256::from(18))?
+                Auditor::deploy(deployer.clone(), U256::from(18))?
                     .send()
                     .await?
                     .address(),
@@ -28,15 +33,104 @@ pub async fn main() -> Result<()> {
         .send()
         .await?
         .address(),
-        client.clone(),
+        deployer.clone(),
     );
     auditor
         .initialize(LiquidationIncentive {
-            liquidator: 90_000_000_000_000_000,
-            lenders: 10_000_000_000_000_000,
+            liquidator: U128::exp10(16).as_u128() * 9,
+            lenders: U128::exp10(16).as_u128(),
         })
         .send()
         .await?
         .await?;
+
+    let irm = InterestRateModel::deploy(
+        deployer.clone(),
+        (
+            U256::exp10(15) * 23,
+            I256::exp10(14) * -25,
+            U256::exp10(16) * 102,
+            U256::exp10(15) * 23,
+            I256::exp10(14) * -25,
+            U256::exp10(16) * 102,
+        ),
+    )?
+    .send()
+    .await?;
+
+    let asset = MockERC20::deploy(
+        deployer.clone(),
+        ("DAI".to_string(), "DAI".to_string(), U256::from(18)),
+    )?
+    .send()
+    .await?;
+
+    let market = Market::new(
+        ERC1967Proxy::deploy(
+            deployer.clone(),
+            (
+                Market::deploy(deployer.clone(), (asset.address(), auditor.address()))?
+                    .send()
+                    .await?
+                    .address(),
+                Bytes::default(),
+            ),
+        )?
+        .send()
+        .await?
+        .address(),
+        deployer.clone(),
+    );
+    market
+        .initialize(
+            6,
+            U128::exp10(18).as_u128() * 2,
+            irm.address(),
+            U256::exp10(16) * 2 / (24 * 3_600),
+            U256::exp10(17),
+            U128::exp10(17).as_u128(),
+            U256::exp10(14) * 46,
+            U256::exp10(16) * 42,
+        )
+        .send()
+        .await?
+        .await?;
+
+    let price_feed = MockPriceFeed::deploy(deployer.clone(), (U256::from(18), I256::exp10(18)))?
+        .send()
+        .await?;
+    auditor
+        .enable_market(
+            market.address(),
+            price_feed.address(),
+            U128::exp10(17).as_u128() * 9,
+        )
+        .send()
+        .await?
+        .await?;
+
+    let alice = RevmMiddleware::new(&environment, None)?;
+    asset
+        .mint(alice.address(), U256::exp10(24))
+        .send()
+        .await?
+        .await?;
+    MockERC20::new(asset.address(), alice.clone())
+        .approve(market.address(), U256::MAX)
+        .send()
+        .await?
+        .await?;
+    let alice_market = Market::new(market.address(), alice.clone());
+    alice_market
+        .deposit(U256::exp10(20), alice.address())
+        .send()
+        .await?
+        .await?;
+    alice_market
+        .borrow(U256::exp10(19), alice.address(), alice.address())
+        .send()
+        .await?
+        .await?;
+
     Ok(())
 }
