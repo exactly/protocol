d21ffab1056c1da05f72698ad91df9b94c04cfd3
itofarina <farinalvaro@gmail.com>
2023-12-13 19:05:30 -0300

    📌 solidity: relax version and set compiler to `0.8.23`

--- contracts/PriceFeedPool.sol
+++ contracts/PriceFeedPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 import { ERC20 } from "solmate/src/tokens/ERC20.sol";
 import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
