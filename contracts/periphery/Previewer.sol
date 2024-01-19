c4165940c858528a5eba33b171448a78d511278f
sebipap <sebipaps@gmail.com>
2024-01-19 12:09:50 -0300

    ✨ previewer: update utilizations and parameters

--- contracts/periphery/Previewer.sol
+++ contracts/periphery/Previewer.sol
@@ -41,6 +41,8 @@ contract Previewer {
     RewardRate[] rewardRates;
     uint256 floatingBorrowRate;
     uint256 floatingUtilization;
+    uint256 floatingAssets;
+    uint256 floatingDebt;
     uint256 floatingBackupBorrowed;
     uint256 floatingAvailableAssets;
     uint256 totalFloatingBorrowAssets;
@@ -82,6 +84,13 @@ contract Previewer {
     int256 curveB;
     uint256 maxUtilization;
     uint256 naturalUtilization;
+    int256 growthSpeed;
+    int256 sigmoidSpeed;
+    int256 spreadFactor;
+    int256 maturitySpeed;
+    int256 timePreference;
+    uint256 fixedAllocation;
+    uint256 maxRate;
   }
 
   struct FixedPosition {
@@ -144,7 +153,14 @@ contract Previewer {
           curveA: irm.floatingCurveA(),
           curveB: irm.floatingCurveB(),
           maxUtilization: irm.floatingMaxUtilization(),
-          naturalUtilization: irm.naturalUtilization()
+          naturalUtilization: irm.naturalUtilization(),
+          growthSpeed: irm.growthSpeed(),
+          sigmoidSpeed: irm.sigmoidSpeed(),
+          spreadFactor: irm.spreadFactor(),
+          maturitySpeed: irm.maturitySpeed(),
+          timePreference: irm.timePreference(),
+          fixedAllocation: irm.fixedAllocation(),
+          maxRate: irm.maxRate()
         }),
         usdPrice: auditor.assetPrice(m.priceFeed).mulWadDown(basePrice),
         penaltyRate: market.penaltyRate(),
@@ -156,6 +172,8 @@ contract Previewer {
         floatingUtilization: market.floatingAssets() > 0
           ? Math.min(market.floatingDebt().divWadUp(market.floatingAssets()), 1e18)
           : 0,
+        floatingAssets: market.floatingAssets(),
+        floatingDebt: market.floatingDebt(),
         floatingBackupBorrowed: market.floatingBackupBorrowed(),
         floatingAvailableAssets: floatingAvailableAssets(market),
         totalFloatingBorrowAssets: market.totalFloatingBorrowAssets(),
