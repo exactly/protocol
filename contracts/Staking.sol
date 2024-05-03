1d4f1c41ba9027414faa0e4d3b5508c78a831954
itofarina <farinalvaro@gmail.com>
2024-05-03 10:23:00 -0300

    🎨 staking: format code

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -13,7 +13,9 @@ contract Staking is Initializable, AccessControlUpgradeable, PausableUpgradeable
   using FixedPointMathLib for uint256;
   using SafeTransferLib for ERC20;
 
+  /// @notice Staking token
   ERC20 public immutable exa;
+  /// @notice Rewards token
   ERC20 public immutable rewardsToken;
 
   /// @notice Duration of rewards to be paid out (in seconds)
@@ -24,12 +26,24 @@ contract Staking is Initializable, AccessControlUpgradeable, PausableUpgradeable
   uint256 public updatedAt;
   /// @notice Reward to be paid out per second
   uint256 public rewardRate;
-  /// @notice Sum of (reward rate * dt * 1e18 / total supply)
-  uint256 public rewardPerTokenStored;
-  mapping(address account => uint256 rewardPerTokenStored) public userRewardPerTokenPaid;
+  /// @notice Global index. Sum of (reward rate * dt * 1e18 / total supply)
+  uint256 public index;
+
+  /// @notice Accounts indexes
+  mapping(address account => uint256 index) public indexes;
+  /// @notice Rewards accrued per account
   mapping(address account => uint256 amount) public rewards;
+  /// @notice Total amount of tokens staked
   uint256 public totalSupply;
+  /// @notice Staked amount per account
   mapping(address account => uint256 amount) public balanceOf;
+
+  /// @notice Penalty for early unstake
+  uint256 public discountFactor;
+  /// @notice Reference period to stake and get full rewards
+  uint256 public refTime;
+  /// @notice Average starting time with the tokens staked per account
+  mapping(address account => uint256 time) public avgStart;
 
   constructor(ERC20 exa_, ERC20 rewardsToken_) {
     exa = exa_;
@@ -44,13 +58,13 @@ contract Staking is Initializable, AccessControlUpgradeable, PausableUpgradeable
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
   }
 
-  modifier updateReward(address _account) {
-    rewardPerTokenStored = rewardPerToken();
+  modifier updateReward(address account) {
+    index = globalIndex();
     updatedAt = lastTimeRewardApplicable();
 
-    if (_account != address(0)) {
-      rewards[_account] = earned(_account);
-      userRewardPerTokenPaid[_account] = rewardPerTokenStored;
+    if (account != address(0)) {
+      rewards[account] = earned(account);
+      indexes[account] = index;
     }
 
     _;
@@ -60,32 +74,42 @@ contract Staking is Initializable, AccessControlUpgradeable, PausableUpgradeable
     return Math.min(finishAt, block.timestamp);
   }
 
-  function rewardPerToken() public view returns (uint256) {
-    if (totalSupply == 0) return rewardPerTokenStored;
+  function globalIndex() public view returns (uint256) {
+    if (totalSupply == 0) return index;
 
-    return rewardPerTokenStored + (rewardRate * (lastTimeRewardApplicable() - updatedAt)).divWadDown(totalSupply);
+    return index + (rewardRate * (lastTimeRewardApplicable() - updatedAt)).divWadDown(totalSupply);
   }
 
-  function stake(uint256 _amount) external updateReward(msg.sender) {
-    if (_amount == 0) revert ZeroAmount();
-    exa.transferFrom(msg.sender, address(this), _amount);
-    balanceOf[msg.sender] += _amount;
-    totalSupply += _amount;
+  function stake(uint256 amount) external updateReward(msg.sender) {
+    if (amount == 0) revert ZeroAmount();
+    exa.transferFrom(msg.sender, address(this), amount);
+    uint256 balance = balanceOf[msg.sender];
+    uint256 weight = balance.divWadDown(balance + amount);
+    avgStart[msg.sender] = avgStart[msg.sender] * weight + (block.timestamp) * (1 - weight);
 
-    emit Stake(msg.sender, _amount);
+    balanceOf[msg.sender] += amount;
+    totalSupply += amount;
+
+    emit Stake(msg.sender, amount);
   }
 
-  function withdraw(uint256 _amount) external updateReward(msg.sender) {
-    if (_amount == 0) revert ZeroAmount();
-    balanceOf[msg.sender] -= _amount;
-    totalSupply -= _amount;
-    exa.transfer(msg.sender, _amount);
+  function withdraw(uint256 amount) external updateReward(msg.sender) {
+    if (amount == 0) revert ZeroAmount();
+    balanceOf[msg.sender] -= amount;
+    totalSupply -= amount;
+    exa.transfer(msg.sender, amount);
 
-    emit Withdraw(msg.sender, _amount);
+    emit Withdraw(msg.sender, amount);
   }
 
-  function earned(address _account) public view returns (uint256) {
-    return balanceOf[_account].mulWadDown(rewardPerToken() - userRewardPerTokenPaid[_account]) + rewards[_account];
+  /// @notice Calculate the amount of rewards that an account has earned but not yet claimed.
+  /// @param account The address of the user for whom to calculate the rewards.
+  /// @return The total amount of earned rewards for the specified user.
+  /// @dev Computes earned rewards by taking the product of the account's balance and the difference between the
+  /// global reward per token and the reward per token already paid to the user.
+  /// This result is then added to any rewards that have already been accumulated but not yet paid out.
+  function earned(address account) public view returns (uint256) {
+    return balanceOf[account].mulWadDown(globalIndex() - indexes[account]) + rewards[account];
   }
 
   function getReward() external updateReward(msg.sender) {
@@ -98,19 +122,19 @@ contract Staking is Initializable, AccessControlUpgradeable, PausableUpgradeable
     }
   }
 
-  function setRewardsDuration(uint256 _duration) external onlyRole(DEFAULT_ADMIN_ROLE) {
+  function setRewardsDuration(uint256 duration_) external onlyRole(DEFAULT_ADMIN_ROLE) {
     if (finishAt > block.timestamp) revert NotFinished();
-    duration = _duration;
+    duration = duration_;
 
-    emit RewardsDurationSet(msg.sender, _duration);
+    emit RewardsDurationSet(msg.sender, duration_);
   }
 
-  function notifyRewardAmount(uint256 _amount) external onlyRole(DEFAULT_ADMIN_ROLE) updateReward(address(0)) {
+  function notifyRewardAmount(uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) updateReward(address(0)) {
     if (block.timestamp >= finishAt) {
-      rewardRate = _amount / duration;
+      rewardRate = amount / duration;
     } else {
       uint256 remainingRewards = (finishAt - block.timestamp) * rewardRate;
-      rewardRate = (_amount + remainingRewards) / duration;
+      rewardRate = (amount + remainingRewards) / duration;
     }
 
     if (rewardRate == 0) revert ZeroRate();
@@ -119,7 +143,7 @@ contract Staking is Initializable, AccessControlUpgradeable, PausableUpgradeable
     finishAt = block.timestamp + duration;
     updatedAt = block.timestamp;
 
-    emit RewardAmountNotified(msg.sender, _amount);
+    emit RewardAmountNotified(msg.sender, amount);
   }
 
   event Stake(address indexed account, uint256 amount);
