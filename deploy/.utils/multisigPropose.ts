bc98d4b6167e4aefbf0fb6abba596d1a532f3449
danilo neves cruz <cruzdanilo@gmail.com>
2025-03-05 19:33:49 -0300

    🔨 deploy: handle safe api failures

--- deploy/.utils/multisigPropose.ts
+++ deploy/.utils/multisigPropose.ts
@@ -17,28 +17,40 @@ export default async (
   });
   const to = await contract.getAddress();
   const data = contract.interface.encodeFunctionData(functionName, args);
-  if (!(await safeApiKit.getPendingTransactions(safeAddress)).results.find((tx) => tx.to === to && tx.data === data)) {
-    // eslint-disable-next-line no-console
-    console.log(`${safe}: proposing`, `${await format(to)}.${functionName}`, await format(args));
-    const safeSdk = await Safe.init({ safeAddress, signer: senderAddress, provider: network.provider });
-    if (!(await safeSdk.isOwner(senderAddress))) {
+  try {
+    if (
+      !(await safeApiKit.getPendingTransactions(safeAddress)).results.find((tx) => tx.to === to && tx.data === data)
+    ) {
       // eslint-disable-next-line no-console
-      console.log(`${safe}: manual proposal`, { to, data, value: "0" });
-      return;
+      console.log(`${safe}: proposing`, `${await format(to)}.${functionName}`, await format(args));
+      const safeSdk = await Safe.init({ safeAddress, signer: senderAddress, provider: network.provider });
+      if (!(await safeSdk.isOwner(senderAddress))) return printManual(safe, to, data);
+      const safeTransaction = await safeSdk.createTransaction({
+        transactions: [{ to, data, value: "0" }],
+        options: { nonce: await safeApiKit.getNextNonce(safeAddress) },
+      });
+      const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
+      const senderSignature = await safeSdk.signHash(safeTxHash);
+      await safeApiKit.proposeTransaction({
+        safeTxHash,
+        safeAddress,
+        senderAddress,
+        senderSignature: senderSignature.data,
+        safeTransactionData: safeTransaction.data,
+        origin: "deploy",
+      });
     }
-    const safeTransaction = await safeSdk.createTransaction({
-      transactions: [{ to, data, value: "0" }],
-      options: { nonce: await safeApiKit.getNextNonce(safeAddress) },
-    });
-    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
-    const senderSignature = await safeSdk.signHash(safeTxHash);
-    await safeApiKit.proposeTransaction({
-      safeTxHash,
-      safeAddress,
-      senderAddress,
-      senderSignature: senderSignature.data,
-      safeTransactionData: safeTransaction.data,
-      origin: "deploy",
-    });
+  } catch (error) {
+    if (account !== safe) {
+      // eslint-disable-next-line no-console
+      console.log(`${safe}: error`, error instanceof Error ? error.message : error);
+      return printManual(safe, to, data);
+    }
+    throw error;
   }
 };
+
+function printManual(safe: string, to: string, data: string) {
+  // eslint-disable-next-line no-console
+  console.log(`${safe}: manual proposal`, { to, data, value: "0" });
+}
