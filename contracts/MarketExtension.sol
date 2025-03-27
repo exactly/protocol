216f9f48e82c8e1a1171fee97c1cad85e8a4b882
Santiago Sanchez Avalos <santisanchezavalos@gmail.com>
2025-03-27 17:04:10 -0300

    ♻️ market: rename fixed borrow threshold setter

--- contracts/MarketExtension.sol
+++ contracts/MarketExtension.sol
@@ -37,7 +37,7 @@ contract MarketExtension is MarketBase {
     setBackupFeeRate(p.backupFeeRate);
     setReserveFactor(p.reserveFactor);
     setDampSpeed(p.floatingAssetsDampSpeedUp, p.floatingAssetsDampSpeedDown, p.uDampSpeedUp, p.uDampSpeedDown);
-    setFixedBorrowThreshold(p.fixedBorrowThreshold, p.curveFactor, p.minThresholdFactor);
+    setFixedBorrowFactors(p.fixedBorrowThreshold, p.curveFactor, p.minThresholdFactor);
   }
 
   function transfer(address to, uint256 shares) public virtual override whenNotPaused returns (bool) {
