a33206d72dee6567448c1da5dff7577844b4c009
itofarina <farinalvaro@gmail.com>
2025-05-23 11:31:45 +0100

    ✨ verified: add verified market

---
+++ contracts/verified/VerifiedMarket.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.17;
+
+import { Auditor, ERC20, Market, RewardsController } from "../Market.sol";
+
+contract VerifiedMarket is Market {
+  constructor(ERC20 asset_, Auditor auditor_) Market(asset_, auditor_) {}
+
+  function handleRewards(bool, address) internal override {} // solhint-disable-line no-empty-blocks
+
+  function setRewardsController(RewardsController) public override {} // solhint-disable-line no-empty-blocks
+
+  function rewardsController() external pure override returns (RewardsController) {
+    return RewardsController(address(0));
+  }
+}
