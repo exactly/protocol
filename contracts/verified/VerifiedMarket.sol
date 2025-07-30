9d288b9d0899b064f863d709b9f6fce3a6bd0b10
itofarina <farinalvaro@gmail.com>
2025-07-30 14:04:48 -0300

    ✨ verified: firewall deposit at maturity

--- contracts/verified/VerifiedMarket.sol
+++ contracts/verified/VerifiedMarket.sol
@@ -1,8 +1,27 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.17;
 
-import { Auditor, ERC20, Market } from "../Market.sol";
+import { ERC20, Market } from "../Market.sol";
+import { VerifiedAuditor } from "./VerifiedAuditor.sol";
 
 contract VerifiedMarket is Market {
-  constructor(ERC20 asset_, Auditor auditor_) Market(asset_, auditor_) {}
+  constructor(ERC20 asset_, VerifiedAuditor auditor_) Market(asset_, auditor_) {}
+
+  function depositAtMaturity(
+    uint256 maturity,
+    uint256 assets,
+    uint256 minAssetsRequired,
+    address receiver
+  ) public override returns (uint256) {
+    if (!isAllowed(msg.sender)) revert NotAllowed(msg.sender);
+    if (!isAllowed(receiver)) revert NotAllowed(receiver);
+
+    return super.depositAtMaturity(maturity, assets, minAssetsRequired, receiver);
+  }
+
+  function isAllowed(address account) internal view returns (bool) {
+    return VerifiedAuditor(address(auditor)).firewall().isAllowed(account);
+  }
 }
+
+error NotAllowed(address account);
