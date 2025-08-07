b0a5e662f8840106267139c39ab205e84959ced1
itofarina <farinalvaro@gmail.com>
2025-08-07 15:57:10 -0300

    ✨ verified: restrict unallows to original granter

--- test/Firewall.t.sol
+++ test/Firewall.t.sol
@@ -5,16 +5,19 @@ import { Test } from "forge-std/Test.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
 
-import { Firewall } from "../contracts/verified/Firewall.sol";
+import { AlreadyAllowed, Firewall, NotGranter } from "../contracts/verified/Firewall.sol";
 
 contract FirewallTest is Test {
   Firewall internal firewall;
+  address internal bob;
 
   function setUp() external {
     firewall = Firewall(address(new ERC1967Proxy(address(new Firewall()), "")));
     firewall.initialize();
     vm.label(address(firewall), "Firewall");
     firewall.grantRole(firewall.GRANTER_ROLE(), address(this));
+
+    bob = makeAddr("bob");
   }
 
   // solhint-disable func-name-mixedcase
@@ -28,14 +31,22 @@ contract FirewallTest is Test {
     assertTrue(allowed);
   }
 
+  function test_allow_disallows_whenGranter() external {
+    firewall.allow(address(this), true);
+    (address granter, bool allowed) = firewall.allowlist(address(this));
+    assertEq(granter, address(this));
+    assertTrue(allowed);
+
+    firewall.allow(address(this), false);
+    (granter, allowed) = firewall.allowlist(address(this));
+    assertEq(granter, address(this));
+    assertFalse(allowed);
+  }
+
   function test_allow_reverts_whenNotGranter() external {
-    vm.startPrank(address(0x1));
+    vm.startPrank(bob);
     vm.expectRevert(
-      abi.encodeWithSelector(
-        IAccessControl.AccessControlUnauthorizedAccount.selector,
-        address(0x1),
-        firewall.GRANTER_ROLE()
-      )
+      abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, bob, firewall.GRANTER_ROLE())
     );
     firewall.allow(address(this), true);
 
@@ -47,6 +58,36 @@ contract FirewallTest is Test {
     vm.expectEmit(true, true, true, true, address(firewall));
     emit AllowlistSet(address(this), address(this), true);
     firewall.allow(address(this), true);
+  }
+
+  function test_allow_reverts_whenAlreadyAllowed() external {
+    firewall.allow(address(this), true);
+    vm.expectRevert(abi.encodeWithSelector(AlreadyAllowed.selector, address(this)));
+    firewall.allow(address(this), true);
+  }
+
+  function test_allow_reverts_whenNotOriginalGranter() external {
+    address granter2 = makeAddr("granter2");
+    firewall.allow(bob, true);
+    firewall.grantRole(firewall.GRANTER_ROLE(), granter2);
+
+    vm.startPrank(granter2);
+    vm.expectRevert(abi.encodeWithSelector(NotGranter.selector, bob, granter2));
+    firewall.allow(bob, false);
+  }
+
+  function test_allow_disallowed_allows_evenWhenNotOriginalGranter() external {
+    address granter2 = makeAddr("granter2");
+    firewall.grantRole(firewall.GRANTER_ROLE(), granter2);
+
+    firewall.allow(bob, true);
+    firewall.allow(bob, false);
+
+    vm.startPrank(granter2);
+    firewall.allow(bob, true);
+    (address granter, bool allowed) = firewall.allowlist(bob);
+    assertEq(granter, granter2);
+    assertTrue(allowed);
   }
 
   // solhint-enable func-name-mixedcase
