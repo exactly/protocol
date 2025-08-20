0246fd70854ff16b668a0a33905be30fc50af703
danilo neves cruz <cruzdanilo@gmail.com>
2025-08-20 10:12:37 +0200

    🔨 deploy: format roles offline

--- deploy/.utils/format.ts
+++ deploy/.utils/format.ts
@@ -1,5 +1,6 @@
 import { inspect } from "util";
 import { deployments, ethers, getNamedAccounts } from "hardhat";
+import { keccak256, toUtf8Bytes, ZeroHash } from "ethers";
 
 const { isAddress, isHexString, Contract, provider } = ethers;
 
@@ -18,6 +19,29 @@ export default async function format(v: unknown): Promise<unknown> {
     }
 
     if (isHexString(v, 32)) {
+      switch (v) {
+        case ZeroHash:
+          return "DEFAULT_ADMIN_ROLE";
+        case keccak256(toUtf8Bytes("ALLOWER_ROLE")):
+          return "ALLOWER_ROLE";
+        case keccak256(toUtf8Bytes("CANCELLER_ROLE")):
+          return "CANCELLER_ROLE";
+        case keccak256(toUtf8Bytes("EMERGENCY_ADMIN_ROLE")):
+          return "EMERGENCY_ADMIN_ROLE";
+        case keccak256(toUtf8Bytes("EXECUTOR_ROLE")):
+          return "EXECUTOR_ROLE";
+        case keccak256(toUtf8Bytes("PAUSER_ROLE")):
+          return "PAUSER_ROLE";
+        case keccak256(toUtf8Bytes("PROPOSER_ROLE")):
+          return "PROPOSER_ROLE";
+        case keccak256(toUtf8Bytes("REDEEMER_ROLE")):
+          return "REDEEMER_ROLE";
+        case keccak256(toUtf8Bytes("TIMELOCK_ADMIN_ROLE")):
+          return "TIMELOCK_ADMIN_ROLE";
+        case keccak256(toUtf8Bytes("TRANSFERRER_ROLE")):
+          return "TRANSFERRER_ROLE";
+      }
+
       for (const { address, abi } of Object.values(allDeployments)) {
         if (!abi) continue;
 
