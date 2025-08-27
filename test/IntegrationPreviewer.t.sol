6477a70afdf52fd8224fbad4cc70fee2848e1358
danilo neves cruz <cruzdanilo@gmail.com>
2025-08-27 11:52:11 +0200

    ✨ integration: preview borrow limit

--- test/IntegrationPreviewer.t.sol
+++ test/IntegrationPreviewer.t.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
 
-import { IntegrationPreviewer, Auditor, Market } from "../contracts/periphery/IntegrationPreviewer.sol";
+import { Auditor, Market, InsufficientAccountLiquidity } from "../contracts/Auditor.sol";
+import { IntegrationPreviewer } from "../contracts/periphery/IntegrationPreviewer.sol";
 import { ForkTest } from "./Fork.t.sol";
 
 contract IntegrationPreviewerTest is ForkTest {
@@ -20,6 +21,7 @@ contract IntegrationPreviewerTest is ForkTest {
     vm.createSelectFork(vm.envString("OPTIMISM_NODE"), 122_565_907);
 
     auditor = Auditor(deployment("Auditor"));
+    exaUSDC = Market(deployment("MarketUSDC"));
     exaUSDC = Market(deployment("MarketUSDC"));
     previewer = IntegrationPreviewer(address(new ERC1967Proxy(address(new IntegrationPreviewer(auditor)), "")));
     deployment("RewardsController");
@@ -45,6 +47,15 @@ contract IntegrationPreviewerTest is ForkTest {
   function test_healthFactor() external view {
     assertEq(previewer.healthFactor(USER), 1.135305841449548453e18);
     assertEq(previewer.healthFactor(address(0)), type(uint256).max);
+  }
+
+  function test_borrowLimit() external {
+    vm.startPrank(USER);
+    exaUSDC.borrow(previewer.borrowLimit(USER, exaUSDC, 1e18) - 1, USER, USER);
+    assertEq(previewer.borrowLimit(USER, exaUSDC, 1e18), 0);
+
+    vm.expectRevert(InsufficientAccountLiquidity.selector);
+    exaUSDC.borrow(1, USER, USER);
   }
 
   // solhint-enable func-name-mixedcase
