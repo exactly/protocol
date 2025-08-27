f82323c737e6ce3afa36d5d1b877bcba5b176f09
danilo neves cruz <cruzdanilo@gmail.com>
2025-08-27 11:52:11 +0200

    ✨ integration: preview borrow limit

--- contracts/periphery/IntegrationPreviewer.sol
+++ contracts/periphery/IntegrationPreviewer.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
 
-import { Auditor, Market } from "../Auditor.sol";
+import { Auditor, Market, IPriceFeed } from "../Auditor.sol";
 
 contract IntegrationPreviewer {
   using FixedPointMathLib for uint256;
@@ -20,5 +20,14 @@ contract IntegrationPreviewer {
     (uint256 adjustedCollateral, uint256 adjustedDebt) = auditor.accountLiquidity(account, Market(address(0)), 0);
     if (adjustedDebt == 0) return type(uint256).max;
     return adjustedCollateral.divWadDown(adjustedDebt);
+  }
+
+  function borrowLimit(address account, Market market, uint256 targetHealthFactor) external view returns (uint256) {
+    (uint256 adjustedCollateral, uint256 adjustedDebt) = auditor.accountLiquidity(account, market, 0);
+    (uint256 adjustFactor, uint256 decimals, , , IPriceFeed priceFeed) = auditor.markets(market);
+    uint256 maxAdjustedDebt = adjustedCollateral.divWadDown(targetHealthFactor);
+    if (adjustedDebt >= maxAdjustedDebt) return 0;
+    uint256 maxExtraDebt = maxAdjustedDebt - adjustedDebt;
+    return maxExtraDebt.mulWadDown(adjustFactor).mulDivDown(10 ** decimals, auditor.assetPrice(priceFeed));
   }
 }
