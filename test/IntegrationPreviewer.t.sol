1a904458cf85d4c793d53c16f5c4a11ee43e233e
danilo neves cruz <cruzdanilo@gmail.com>
2025-10-22 13:48:29 -0300

    ✅ integration: update test block and account

--- test/IntegrationPreviewer.t.sol
+++ test/IntegrationPreviewer.t.sol
@@ -6,25 +6,29 @@ import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
 
 import { Auditor, Market, InsufficientAccountLiquidity } from "../contracts/Auditor.sol";
 import { IntegrationPreviewer } from "../contracts/periphery/IntegrationPreviewer.sol";
+import { InterestRateModel } from "../contracts/InterestRateModel.sol";
+import { RewardsController } from "../contracts/RewardsController.sol";
 import { ForkTest } from "./Fork.t.sol";
 
 contract IntegrationPreviewerTest is ForkTest {
   using FixedPointMathLib for uint256;
 
-  address internal constant USER = 0x291019ecdA53f4E841f6722fEf239C9Dd120e6d5;
+  address internal constant USER = 0x738E079c7c9009040A265daa1cF53F772B19Fb18;
+  uint256 internal constant MATURITY = 1_763_596_800;
 
+  address internal timelock;
   Auditor internal auditor;
   Market internal exaUSDC;
   IntegrationPreviewer internal previewer;
 
   function setUp() external {
-    vm.createSelectFork(vm.envString("OPTIMISM_NODE"), 122_565_907);
+    vm.createSelectFork(vm.envString("OPTIMISM_NODE"), 142_772_987);
 
+    timelock = deployment("TimelockController");
     auditor = Auditor(deployment("Auditor"));
     exaUSDC = Market(deployment("MarketUSDC"));
     exaUSDC = Market(deployment("MarketUSDC"));
     previewer = IntegrationPreviewer(address(new ERC1967Proxy(address(new IntegrationPreviewer(auditor)), "")));
-    deployment("RewardsController");
     deployment("PriceFeedOP");
     deployment("PriceFeedUSDC");
     deployment("PriceFeedUSDC.e");
@@ -41,12 +45,22 @@ contract IntegrationPreviewerTest is ForkTest {
     vm.label(address(Market(deployment("MarketUSDC.e")).interestRateModel()), "InterestRateModelUSDC.e");
     vm.label(address(Market(deployment("MarketWBTC")).interestRateModel()), "InterestRateModelWBTC");
     vm.label(address(Market(deployment("MarketWETH")).interestRateModel()), "InterestRateModelWETH");
+
+    upgrade(deployment("RewardsController"), address(new RewardsController()));
+    Market[] memory markets = auditor.allMarkets();
+    for (uint256 i = 0; i < markets.length; ++i) {
+      upgrade(address(markets[i]), address(new Market(markets[i].asset(), auditor)));
+      vm.startPrank(timelock);
+      markets[i].initialize2(type(uint256).max);
+      markets[i].setInterestRateModel(new InterestRateModel(markets[i].interestRateModel().parameters(), markets[i]));
+      vm.stopPrank();
+    }
   }
 
   // solhint-disable func-name-mixedcase
 
   function test_healthFactor() external view {
-    assertEq(previewer.healthFactor(USER), 1.135305841449548453e18);
+    assertEq(previewer.healthFactor(USER), 1.167689639007889221e18);
     assertEq(previewer.healthFactor(address(0)), type(uint256).max);
   }
 
@@ -60,115 +74,104 @@ contract IntegrationPreviewerTest is ForkTest {
   }
 
   function test_fixedRepayAssets_beforeMaturity() external {
-    uint256 maturity = 1_734_566_400;
     uint256 positionAssets = 420e6;
 
-    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, maturity, positionAssets);
+    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, MATURITY, positionAssets);
 
     vm.startPrank(USER);
     deal(address(exaUSDC.asset()), USER, 1_000_000e6);
     exaUSDC.asset().approve(address(exaUSDC), type(uint256).max);
-    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(maturity, positionAssets, type(uint256).max, USER);
+    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(MATURITY, positionAssets, type(uint256).max, USER);
 
     assertEq(actualRepayAssets, repayAssets);
   }
 
   function test_fixedRepayAssets_afterMaturity() external {
-    uint256 maturity = 1_734_566_400;
     uint256 positionAssets = 420e6;
-
     skip(55 weeks);
 
-    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, maturity, positionAssets);
+    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, MATURITY, positionAssets);
 
     vm.startPrank(USER);
     deal(address(exaUSDC.asset()), USER, 1_000_000e6);
     exaUSDC.asset().approve(address(exaUSDC), type(uint256).max);
-    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(maturity, positionAssets, type(uint256).max, USER);
+    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(MATURITY, positionAssets, type(uint256).max, USER);
 
     assertEq(actualRepayAssets, repayAssets);
   }
 
   function test_fixedRepayAssets_maxUintBeforeMaturity() external {
-    uint256 maturity = 1_734_566_400;
     uint256 positionAssets = type(uint256).max;
 
-    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, maturity, positionAssets);
+    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, MATURITY, positionAssets);
 
     vm.startPrank(USER);
     deal(address(exaUSDC.asset()), USER, 1_000_000e6);
     exaUSDC.asset().approve(address(exaUSDC), type(uint256).max);
-    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(maturity, positionAssets, type(uint256).max, USER);
+    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(MATURITY, positionAssets, type(uint256).max, USER);
 
     assertEq(actualRepayAssets, repayAssets);
   }
 
   function test_fixedRepayAssets_maxUintAfterMaturity() external {
-    uint256 maturity = 1_734_566_400;
     uint256 positionAssets = type(uint256).max;
-
     skip(55 weeks);
 
-    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, maturity, positionAssets);
+    uint256 repayAssets = previewer.fixedRepayAssets(USER, exaUSDC, MATURITY, positionAssets);
 
     vm.startPrank(USER);
     deal(address(exaUSDC.asset()), USER, 1_000_000e6);
     exaUSDC.asset().approve(address(exaUSDC), type(uint256).max);
-    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(maturity, positionAssets, type(uint256).max, USER);
+    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(MATURITY, positionAssets, type(uint256).max, USER);
 
     assertEq(actualRepayAssets, repayAssets);
   }
 
   function test_fixedRepayPosition_beforeMaturity() external {
-    uint256 maturity = 1_734_566_400;
     uint256 repayAssets = 420e6;
 
-    uint256 positionAssets = previewer.fixedRepayPosition(USER, exaUSDC, maturity, repayAssets);
+    uint256 positionAssets = previewer.fixedRepayPosition(USER, exaUSDC, MATURITY, repayAssets);
 
     vm.startPrank(USER);
     deal(address(exaUSDC.asset()), USER, 1_000_000e6);
     exaUSDC.asset().approve(address(exaUSDC), type(uint256).max);
-    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(maturity, positionAssets, type(uint256).max, USER);
+    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(MATURITY, positionAssets, type(uint256).max, USER);
 
     assertLe(actualRepayAssets, repayAssets);
     assertApproxEqAbs(actualRepayAssets, repayAssets, 1);
   }
 
   function test_fixedRepayPosition_afterMaturity() external {
-    uint256 maturity = 1_734_566_400;
     uint256 repayAssets = 420e6;
-
     skip(55 weeks);
 
-    uint256 positionAssets = previewer.fixedRepayPosition(USER, exaUSDC, maturity, repayAssets);
+    uint256 positionAssets = previewer.fixedRepayPosition(USER, exaUSDC, MATURITY, repayAssets);
 
     vm.startPrank(USER);
     deal(address(exaUSDC.asset()), USER, 1_000_000e6);
     exaUSDC.asset().approve(address(exaUSDC), type(uint256).max);
-    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(maturity, positionAssets, type(uint256).max, USER);
+    uint256 actualRepayAssets = exaUSDC.repayAtMaturity(MATURITY, positionAssets, type(uint256).max, USER);
 
     assertLe(actualRepayAssets, repayAssets);
     assertApproxEqAbs(actualRepayAssets, repayAssets, 1);
   }
 
   function test_fixedRepayPosition_maxUint() external {
-    uint256 maturity = 1_734_566_400;
     uint256 repayAssets = type(uint256).max;
-    (uint256 principal, uint256 fee) = exaUSDC.fixedBorrowPositions(maturity, USER);
+    (uint256 principal, uint256 fee) = exaUSDC.fixedBorrowPositions(MATURITY, USER);
 
-    assertEq(previewer.fixedRepayPosition(USER, exaUSDC, maturity, repayAssets), principal + fee);
+    assertEq(previewer.fixedRepayPosition(USER, exaUSDC, MATURITY, repayAssets), principal + fee);
 
     skip(55 weeks);
 
-    assertEq(previewer.fixedRepayPosition(USER, exaUSDC, maturity, repayAssets), principal + fee);
+    assertEq(previewer.fixedRepayPosition(USER, exaUSDC, MATURITY, repayAssets), principal + fee);
   }
 
   function test_fixedRepayPosition_saturatedFallback() external view {
-    uint256 maturity = 1_734_566_400;
-    uint256 repayAssets = 831_039_892;
-    (uint256 principal, uint256 fee) = exaUSDC.fixedBorrowPositions(maturity, USER);
+    uint256 repayAssets = 13_859_081_618;
+    (uint256 principal, uint256 fee) = exaUSDC.fixedBorrowPositions(MATURITY, USER);
 
-    assertEq(previewer.fixedRepayPosition(USER, exaUSDC, maturity, repayAssets), principal + fee);
+    assertEq(previewer.fixedRepayPosition(USER, exaUSDC, MATURITY, repayAssets), principal + fee);
   }
 
   // solhint-enable func-name-mixedcase
