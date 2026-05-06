4a4db448e33517d01d0d061310b82442be7c6113
itofarina <farinalvaro@gmail.com>
2026-05-06 10:35:53 -0300

    🔧 finance: update cbXRP max supply

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -396,7 +396,7 @@ const hardhatConfig: Config = {
       cbXRP: {
         networks: ["base", "base-sepolia"],
         adjustFactor: 0.6,
-        maxSupply: 1_100_000,
+        maxSupply: 10_500_000,
         interestRateModel: {
           minRate: 1.5e-2,
           naturalRate: 2e-1,
